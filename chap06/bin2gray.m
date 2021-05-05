dd24217e67d38177c6eb38b391154c130955e77b
Thomas Bewley <bewley@eng.ucsd.edu>
2021-05-04 18:17:57 -0700

    added gray code stuff

---
+++ chap06/bin2gray.m
@@ -0,0 +1,10 @@
+function g=bin2gray(b)
+% INPUT:  in=binary number [generate, e.g., via dec2bin(int)]
+% OUTPUT: g=corresponding Gray form
+% EXAMPLE CALL: for d=0:15, b=dec2bin(d); g=bin2gray(b); disp({d b g}), end
+% algorithm from https://www.matrixlab-examples.com/gray-code.html
+
+g(1)=b(1);
+for i=2:length(b);
+    x=xor(str2num(b(i-1)),str2num(b(i))); g(i)=num2str(x);
+end
---
+++ chap06/gray2bin.m
@@ -0,0 +1,10 @@
+function b=gray2bin(g)
+% INPUT:  g=Gray code number
+% OUTPUT: b=corresponding binary number
+% EXAMPLE CALL: d=87, b=dec2bin(d), g=bin2gray(b), b1=gray2bin(g)
+% algorithm from https://www.matrixlab-examples.com/gray-code.html
+
+b(1)=g(1);
+for i=2:length(g)
+    x=xor(str2num(b(i-1)),str2num(g(i))); b(i)=num2str(x);
+end
