2abde0f3351506b2fdd1d78a36b3e4c92f2ad4df
Thomas Bewley <bewley@eng.ucsd.edu>
2021-05-05 17:06:30 -0700

    Update plot_encoder_signals.m

--- chap06/plot_encoder_signals.m
+++ chap06/plot_encoder_signals.m
@@ -6,11 +6,11 @@ figure(1); clf
 plot([0 1 1 2 2 3 3 4 4 5 5 6 6 7],[1 1 0 0 1 1 0 0 1 1 0 0 1 1],'b-','LineWidth',2);
 hold on, axis([0.25 3.75 -0.5 1.5]), axis equal, axis off, 
 plot(-0.5+[0 1 1 2 2 3 3 4 4 5 5 6 6 7],0.09+[0 0 1 1 0 0 1 1 0 0 1 1 0 0],'r--','LineWidth',2)
-print -depsc quad_encoder_signals
+% print -depsc quad_encoder_signals
 
 figure(2); clf
 plot(-1+[0 1 1 2 2 3 3 4 4 5 5 6 6 7],[1 1 0 0 1 1 0 0 1 1 0 0 1 1],'b-','LineWidth',2);
 hold on, axis([2.5/6 27.5/6 -0.5 1.5]), axis equal, axis off,
 plot(-1/3+[0 1 1 2 2 3 3 4 4 5 5 6 6 7],0.09+[0 0 1 1 0 0 1 1 0 0 1 1 0 0],'r--','LineWidth',2)
 plot(1/3+[0 1 1 2 2 3 3 4 4 5 5 6 6 7],-0.09+[0 0 1 1 0 0 1 1 0 0 1 1 0 0],'m-.','LineWidth',2)
-print -depsc UVW_encoder_signals
+% print -depsc UVW_encoder_signals
