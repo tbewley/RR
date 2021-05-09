ecd7ea06b2f04957a5c52841c2e6eb43da8d2691
Thomas Bewley <bewley@eng.ucsd.edu>
2021-05-09 15:13:25 -0700

    a bunch of derived functions from cordic

--- chap01/cordic_derived.m
+++ chap01/cordic_derived.m
@@ -1,8 +1,10 @@
 function out = cordic_derived(func,in,n,cordic_tables)
 % function out = cordic(func,in,n,cordic_tables)
-% Calls cordic (which in turn calls cordic_core) to calculate various derived functions.
+% Calls cordic (which in turn calls cordic_core) to calculate various derived functions,
+% using common identities for log, exponential, trigonometric, and hyperbolic functions.
 % NOTE:   initialize cordic_tables by calling cordic_init script (just once)
-% INPUT:  func={'tan'}
+% INPUT:  func={'tan','tanh','exp','log','log_b','sqrt', ...
+%               'acos','asin','acosh','asinh','power'}
 %         in=input data (as defined for each case in comments below)
 %         n=number of iterations (with increased precision for larger n; try n<40)
 %         cordic_tables=tables of values initialized using cordic_init
@@ -13,55 +15,80 @@ function out = cordic_derived(func,in,n,cordic_tables)
 
 switch func
   case 'tan'
-    % in=angle (any real number)
-    % out=tan(in)
-    % EXAMPLE CALL: x=1.0; cordic_derived('tan',x,40,cordic_tables), tan(x)
+    % out=tan(in) where in is any real number
+    % uses identity tan(x)=sin(x)/cos(x)
+    % EXAMPLE CALL: in=1.0; cordic_derived('tan',in,40,cordic_tables), tan(in)
     v=cordic('cos_sin',in,n,cordic_tables);
     out=v(2)/v(1);
-  case 'mod_atan'
-    % in=[x,y,z] 
-    % out(1)->sqrt(x^2+y^2)
-    % out(2)=error in y
-    % out(3)->z+atan(y/x)
-    % EXAMPLE CALL: cordic('mod_atan',[1.0; 2.0; 3.0],30,cordic_tables)
-    rot=1; mode=2;
-    v=[cordic_tables.K(1,min(n,cordic_tables.N))*[in(1); in(2)]; in(3)];    
-  case 'cosh_sinh'
-    % in=input angle (NOTE! Only works for -1.118173<in<1.118173)
-    % out(1,2)->[cosh(in); sinh(in)]
-    % out(3)=angle error
-    % Example: cordic('cosh_sinh',1.0,30,cordic_tables)
-    rot=2; mode=1;
-    v=[cordic_tables.K(2,min(n,cordic_tables.N)); 0; in];
-  case 'GivensH'
-    % in=[x,y,z] (NOTE! Only works for -1.118173<z<1.118173)
-    % out(1,2)->H*[x;y] with H=[cosh(z) sinh(z); sinh(z) cosh(z)]
-    % out(3)=angle error
-    % EXAMPLE CALL: cordic('GivensH',[1.0; 0.2; 1.0],30,cordic_tables)
-    rot=2; mode=1;
-    v=[cordic_tables.K(2,min(n,cordic_tables.N))*[in(1); in(2)]; in(3)];
-  case 'modh_atanh'
-    % in=[x,y,z] (NOTE! Only works for -1.118173<atanh(y/x)<1.118173)
-    % out(1)->sqrt(x^2-y^2)
-    % out(2)=error in y
-    % out(3)->z+atanh(y/x)
-    % EXAMPLE CALL: cordic('modh_atanh',[1; 0.2; 3.0],30,cordic_tables)
-    rot=2; mode=2;
-    v=[cordic_tables.K(2,min(n,cordic_tables.N))*[in(1); in(2)]; in(3)];    
-  case 'MAC' % (Multiply / Accumulate)
-    % in=[x,y,z] (NOTE! Only works for -2<z*x<2)
-    % out(1)=x
-    % out(2)->y+z*x
-    % out(3)=angle error
-    % EXAMPLE CALL: x=1.1; y=0.1; z=0.6; cordic('MAC',[x;y;z],30,cordic_tables), y+z*x
-    rot=3; mode=1; v=in;
-  case 'DAC'  % (Divide / Accumulate)
-    % in=[x,y,z] (NOTE! Only works for -2<y/x<2)
-    % out(1)=x
-    % out(2)=error in y
-    % out(3)->z+y/x
-    % EXAMPLE CALL: x=1.1; y=0.1; z=0.6; cordic('DAC',[x;y;z],30,cordic_tables), z+y/x
-    rot=3; mode=2; v=in;
+  case 'tanh'
+    % out=tanh(in)
+    % uses identity tanh(x)=sinh(x)/cosh(x)
+    % EXAMPLE CALL: in=1.0; cordic_derived('tanh',in,40,cordic_tables), tanh(in)
+    v=cordic('cosh_sinh',in,n,cordic_tables);
+    out=v(2)/v(1);
+  case 'exp'
+    % out=exp(in)
+    % uses identity exp(x)=sinh(x)+cosh(x)
+    % EXAMPLE CALL: in=1.0; cordic_derived('exp',in,40,cordic_tables), exp(in)
+    v=cordic('cosh_sinh',in,n,cordic_tables);
+    out=v(1)+v(2);
+  case 'log' 
+    % out=log(in)  where log denotes the natural log, aka ln
+    % uses identity log(x)=2*atanh((w-1)/(w+1))
+    % EXAMPLE CALL: in=1.2; cordic_derived('log',in,40,cordic_tables), log(in)
+    v=cordic('modh_atanh',[in+1; in-1; 0],n,cordic_tables);
+    out=2*v(3);
+  case 'log_b'
+    % in=[x,b]
+    % out=log_b(x)
+    % uses identity log_b(x)=log(x)/log(b))
+    % EXAMPLE CALLS:
+    %  x=1.2; b=10; cordic_derived('log_b',[x,b],40,cordic_tables), log10(x)
+    %  x=1.2; b=2;  cordic_derived('log_b',[x,b],40,cordic_tables), log2(x)
+    %  x=1.2; b=5;  cordic_derived('log_b',[x,b],40,cordic_tables), log(x)/log(5)
+    x=in(1); b=in(2); log_x=cordic_derived('log',x,n,cordic_tables);
+    switch b
+       case 2,    log_b=0.693147180559945;
+       case 10,   log_b=2.302585092994046;
+       otherwise, log_b=cordic_derived('log',b,n,cordic_tables);
+    end
+    out=log_x/log_b;
+  case 'sqrt'
+    % out=sqrt(in)
+    % uses identity sqrt(x)=sqrt((x+1/4)^2-(x-1/4)^2)
+    % EXAMPLE CALL: in=0.5; cordic_derived('sqrt',in,40,cordic_tables), sqrt(in)
+    v=cordic('modh_atanh',[in+0.25; in-0.25; 0],n,cordic_tables);
+    out=v(1);
+  case 'acos'
+    % out=acos(in)
+    % uses identity acos(x)=atan(sqrt(1-x^2)/x)
+    % EXAMPLE CALL: in=0.5; cordic_derived('acos',in,40,cordic_tables), acos(in)
+    v=cordic('mod_atan',[in; sqrt(1-in^2); 0],n,cordic_tables);
+    out=v(3);
+  case 'asin'
+    % out=asin(in)
+    % uses identity asin(x)=atan(x/sqrt(1-x^2))  
+    % EXAMPLE CALL: in=0.5; cordic_derived('asin',in,40,cordic_tables), asin(in)
+    v=cordic('mod_atan',[sqrt(1-in^2); in; 0],n,cordic_tables);
+    out=v(3);
+  case 'acosh'
+    % out=acos(in)
+    % uses identity acosh(x)=ln(x+sqrt(x^2-1))
+    % EXAMPLE CALL: in=2; cordic_derived('acosh',in,40,cordic_tables), acosh(in)
+    out=cordic_derived('log',in+sqrt(in^2-1),n,cordic_tables);
+  case 'asinh'
+    % out=asin(in)
+    % uses identity asinh(x)=ln(x+sqrt(x^2+1))
+    % EXAMPLE CALL: in=0.5; cordic_derived('asinh',in,40,cordic_tables), asinh(in)
+    out=cordic_derived('log',in+sqrt(in^2+1),n,cordic_tables);
+  case 'power'
+    % in=[x,y]
+    % out=x^y
+    % uses identity x^y=exp(y*ln(x))
+    % EXAMPLE CALL: x=2; y=0.25; cordic_derived('power',[x,y],40,cordic_tables), x^y
+    x=in(1); y=in(2);
+    ln_x=cordic_derived('log',x,n,cordic_tables);
+    out=cordic_derived('exp',y*ln_x,n,cordic_tables);
   otherwise
     disp('Error: case not yet implemented.')
 end
