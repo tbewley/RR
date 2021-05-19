0fb2856666c787e1f76da50fd8b23d2a0e24bcd0
Thomas Bewley <bewley@eng.ucsd.edu>
2021-05-19 08:54:14 -0700

    Update RR_bin2gray.m

--- chap06/RR_bin2gray.m
+++ chap06/RR_bin2gray.m
@@ -3,7 +3,7 @@ function g=RR_bin2gray(b)
 % Converts from binry to Gray code
 % INPUT:  in=binary number [generate, e.g., via dec2bin(int)]
 % OUTPUT: g=corresponding Gray form
-% EXAMPLE CALL: for d=0:15, b=dec2bin(d); g=RR_bin2gray(b); disp({d b g}), end
+% EXAMPLE CALL: for d=0:31, b=dec2bin(d); g=RR_bin2gray(b); disp({d b g}), end
 % Renaissance Robotics codebase, Chapter 6, https://github.com/tbewley/RR
 % Copyright 2021 by Thomas Bewley, distributed under BSD-3-Clause license.
 
