0189258bf074cd34af6a9d638124b6dddafc37b9
Thomas Bewley <bewley@eng.ucsd.edu>
2021-08-09 13:31:00 -0700

    Update RR_Wheatstone_Capacitors.m

--- chap10/RR_Wheatstone_Capacitors.m
+++ chap10/RR_Wheatstone_Capacitors.m
@@ -3,7 +3,7 @@
 % Renaissance Robotics codebase, Chapter 10, https://github.com/tbewley/RR
 % Copyright 2021 by Thomas Bewley, distributed under Modified BSD License.
 
-syms C5 s V0; R1=1e3; R4=1e3; C2=1e-5; R3=1e5;
+syms C2 C5 s V0; R1=1e3; R4=1e3; R3=1e5;
 A=[1 -1 -1 0 0 0 0 0; 0 1 0 -1 -1 0 0 0; 0 0 1 1 0 -1 0 0; 0 R1 0 0 0 0 1 0; ...
    0 0 1 0 0 0 0 C2*s; 0 0 0 R3 0 0 -1 1; 0 0 0 0 R4 0 -1 0; 0 0 0 0 0 1 0 -C5*s];
 b=[0; 0; 0; V0; C2*s*V0; 0; 0; 0]; x=A\b
