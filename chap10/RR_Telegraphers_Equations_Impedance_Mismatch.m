a479b4a0bcb4a17fcf561f2ada99e693d665463c
Thomas Bewley <bewley@eng.ucsd.edu>
2021-09-02 11:16:46 -0700

    fixed bug in termination on right end.

--- chap10/RR_Telegraphers_Equations_Impedance_Mismatch.m
+++ chap10/RR_Telegraphers_Equations_Impedance_Mismatch.m
@@ -3,30 +3,33 @@
 % Renaissance Robotics codebase, Chapter 10, https://github.com/tbewley/RR
 % Copyright 2021 by Thomas Bewley, distributed under Modified BSD License.
 
-clear; termination=true
-R=0; G=0;                   % also try G=1e-3 or R=1e1
-L=525e-9; C=52e-12; Z0=sqrt(L/C), c=1/sqrt(L*C), X=10; N=200; T=X/c;  h=T/1000;
+clear; % Note: this code is hardwired to use termination on the right end.
+R=0; G=0; % also try G=1e-3 or R=1e1
+L=525e-9; C=52e-12; Z0=sqrt(L/C), c=1/sqrt(L*C), X=10; N=400; T=X/c;  h=T/1000;
 Delta_x=X/(N+0.5); d=1/Delta_x; A=zeros(2*N,2*N);
 figure(1); clf, XV=[0:Delta_x:X]; XI=[Delta_x/2:Delta_x:X];
 for i=1:N/2,
    if i>1, A(2*i-1,2*i-2)= d/L; end, A(2*i-1,2*i-1)=-R/L; A(2*i-1,2*i)=-d/L; 
    if i<N, A(2*i,2*i+1)  =-d/C; end, A(2*i,  2*i  )=-G/C; A(2*i,2*i-1)= d/C;
 end
-Rr=0; Gr=0; Lr=L; Cr=4*C; Z0r=sqrt(Lr/Cr), cr=1/sqrt(Lr*Cr),  % CHANGE L and C IN RIGHT HALF OF WIRE
+
+% IN THE FOLLOWING LINE, WE MODIFY L and/or C IN THE RIGHT HALF OF WIRE
+Rr=0; Gr=0; Lr=4*L; Cr=0.25*C; Z0r=sqrt(Lr/Cr), cr=1/sqrt(Lr*Cr),  
+% [Try Lr=alpha*L, Cr=beta*C for {alpha,beta} some combination of {4,1,0.25}.]
+
 for i=N/2+1:N,
    if i>1, A(2*i-1,2*i-2)= d/Lr; end, A(2*i-1,2*i-1)=-Rr/Lr; A(2*i-1,2*i)=-d/Lr; 
    if i<N, A(2*i,2*i+1)  =-d/Cr; end, A(2*i,  2*i  )=-Gr/Cr; A(2*i,2*i-1)= d/Cr;
 end
-if termination, A(2*N,2*N)=A(2*N,2*N)-(d/C)/Z0r; end
+A(2*N,2*N)=A(2*N,2*N)-(d/Cr)/Z0r; % incorporation of termination 
 D=eye(2*N)-A*h/2; E=eye(2*N)+A*h/2;
-n_max=floor(5.050001*T/h); t1=1e-8; x=zeros(2*N,1); t=0; 
+n_max=floor(3*T/h); t1=1e-8; x=zeros(2*N,1); t=0; 
 for n=1:n_max
    r=E*x; t=(n-0.5)*h;
    if t<t1, Vleft=(1-cos(pi*t/t1)); else, Vleft=2; end, r(1)=r(1)+(d/L)*h*Vleft;
-   x=D\r; t=n*h;
-   if termination, Iright=x(2*N)/Z0r; else, Iright=0; end
+   x=D\r; t=n*h; Iright=x(2*N)/Z0r;
    if mod(n,50)==0, subplot(2,1,1), plot(XV, [Vleft; x(2:2:2*N)]), axis([0 X -0.2 4.2]), 
                     title(['Voltage, t/T = ',num2str(t/T)]), hold on
                     subplot(2,1,2), plot(XI, [x(1:2:2*N-1); Iright]), axis([0 X -.042 .042]),
-                    title(['Current, t/T = ',num2str(t/T)]), hold on, pause(0.4), clf, end
- end
+                    title(['Current, t/T = ',num2str(t/T)]), hold on, if n<n_max, pause(0.1), clf, end, end
+end
