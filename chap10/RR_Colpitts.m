1fdcfadfb7471d17285748111ebf97b1c1351798
Thomas Bewley <bewley@eng.ucsd.edu>
2021-09-13 18:33:50 -0700

    simplifying matlab notation for solves

--- chap10/RR_Colpitts.m
+++ chap10/RR_Colpitts.m
@@ -5,27 +5,25 @@
 
 % Problem 2a - simple LC tank
 clear; syms C1 C2 L3 R4 I Va Vb Vout Vo s
-eqn1='I=-s*C2*Va'; eqn2='Va-Vb=s*L3*I';	eqn3='Vb-Vout=R4*I'; eqn4='I=C1*(s*Vout-Vo)';
-SOL=solve(str2sym(eqn1),str2sym(eqn2),str2sym(eqn3),str2sym(eqn4),I,Va,Vb,Vout); SOL.Vout
+eqn1= I==-s*C2*Va; eqn2= Va-Vb==s*L3*I; eqn3= Vb-Vout==R4*I; eqn4= I==C1*(s*Vout-Vo);
+SOL=solve(eqn1,eqn2,eqn3,eqn4,I,Va,Vb,Vout); SOL.Vout
 pause
 
 % Problem 3b - Colpitts oscillator WITHOUT the extra resistor
 clear; syms C1 C2 L3 R4 R5 R6 I1 I2 I3 I4 I5 I6 I7 Va Vb Vout Vout0 Va0 I30 s
-eqn1 = 'I3=I4';		eqn5 = 'I2=-C2*(s*Va-Va0)';	eqn9 = '-Vout=R6*I6';
-eqn2 = 'I5=I6';		eqn6 = 'Va-Vb=L3*(s*I3-I30)';	eqn10 = 'I1=C1*(s*Vout-Vout0)';
-eqn3 = 'I2=I3+I5';	eqn7 = 'Vb-Vout=R4*I4';		
-eqn4 = 'I4+I6+I7=I1';	eqn8 = 'Va=R5*I5';		
-SOL=solve(str2sym(eqn1),str2sym(eqn2),str2sym(eqn3),str2sym(eqn4),str2sym(eqn5),...
-str2sym(eqn6),str2sym(eqn7),str2sym(eqn8),str2sym(eqn9),str2sym(eqn10),...
+eqn1= I3==I4;		eqn5= I2==-C2*(s*Va-Va0);	eqn9 = -Vout==R6*I6;
+eqn2= I5==I6;		eqn6= Va-Vb==L3*(s*I3-I30);	eqn10= I1==C1*(s*Vout-Vout0);
+eqn3= I2==I3+I5;	eqn7= Vb-Vout==R4*I4;		
+eqn4= I4+I6+I7==I1;	eqn8= Va==R5*I5;		
+SOL=solve(eqn1,eqn2,eqn3,eqn4,eqn5,eqn6,eqn7,eqn8,eqn9,eqn10,...
 I1,I2,I3,I4,I5,I6,I7,Va,Vb,Vout);  SOL.Vout
 pause
 
 % Problem 3c - Colpitts oscillator WITH the extra resistor
 clear; syms C1 C2 L3 R4 R5 R6 R8 I1 I2 I3 I4 I5 I6 I7 I8 Va Vb Vs Vout Vout0 Va0 I30 s
-eqn1 = 'I3=I4';		eqn5 = 'I6+I7=I8';		eqn9 = 'Va=R5*I5';
-eqn2 = 'I5=I6';		eqn6 = 'I2=-C2*(s*Va-Vo)';	eqn10 = '-Vs=R6*I6';
-eqn3 = 'I2=I3+I5';	eqn7 = 'Va-Vb=s*L3*I3';		eqn11 = 'I1=C1*(s*Vout-Vo)';
-eqn4 = 'I4+I8=I1';	eqn8 = 'Vb-Vout=R4*I4';		eqn12 = 'Vs-Vout=R8*I8';
-SOL=solve(str2sym(eqn1),str2sym(eqn2),str2sym(eqn3),str2sym(eqn4),str2sym(eqn5),...
-str2sym(eqn6),str2sym(eqn7),str2sym(eqn8),str2sym(eqn9),str2sym(eqn10),...
-str2sym(eqn11),str2sym(eqn12),I1,I2,I3,I4,I5,I6,I7,I8,Va,Vb,Vs,Vout);  SOL.Vout
+eqn1= I3==I4;		eqn5= I6+I7==I8;			eqn9 = Va==R5*I5;
+eqn2= I5==I6;		eqn6= I2==-C2*(s*Va-Va0);	eqn10= -Vs==R6*I6;
+eqn3= I2==I3+I5;	eqn7= Va-Vb==s*L3*I3;		eqn11= I1==C1*(s*Vout-Vout0);
+eqn4= I4+I8==I1;	eqn8= Vb-Vout==R4*I4;		eqn12= Vs-Vout==R8*I8;
+SOL=solve(eqn1,eqn2,eqn3,eqn4,eqn5,eqn6,eqn7,eqn8,eqn9,eqn10,eqn11,eqn12,...
+I1,I2,I3,I4,I5,I6,I7,I8,Va,Vb,Vs,Vout);  SOL.Vout
--- chap10/RR_Wye_Delta.m
+++ chap10/RR_Wye_Delta.m
@@ -5,11 +5,11 @@
 % Copyright 2021 by Thomas Bewley, distributed under Modified BSD License.
 
 syms R1 R2 R3 R4 R5 Ra Rb Rc
-eqn1='Ra+Rb==1/(1/R1+1/(R2+R3))'
-eqn2='Ra+Rc==1/(1/R2+1/(R1+R3))'
-eqn3='Rb+Rc==1/(1/R3+1/(R1+R2))'
-A=solve(str2sym(eqn1),str2sym(eqn2),str2sym(eqn3),Ra,Rb,Rc);
-B=solve(str2sym(eqn1),str2sym(eqn2),str2sym(eqn3),R1,R2,R3);
+eqn1= Ra+Rb==1/(1/R1+1/(R2+R3))
+eqn2= Ra+Rc==1/(1/R2+1/(R1+R3))
+eqn3= Rb+Rc==1/(1/R3+1/(R1+R2))
+A=solve(eqn1,eqn2,eqn3,Ra,Rb,Rc);
+B=solve(eqn1,eqn2,eqn3,R1,R2,R3);
 Ra=simplify(A.Ra),Rb=simplify(A.Rb),Rc=simplify(A.Rc)
 simplify(Ra + 1/(1/(Rb+R4) + 1/(Rc+R5)))
 R1=simplify(B.R1),R2=simplify(B.R2),R3=simplify(B.R3)
