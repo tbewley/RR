69b83326594d396fd7c3f7dff7de81a70d0f7da1
Thomas Bewley <bewley@eng.ucsd.edu>
2021-09-15 12:35:12 -0700

    Update RR_Boost.m

--- chap10/RR_Boost.m
+++ chap10/RR_Boost.m
@@ -4,8 +4,8 @@
 % Copyright 2021 by Thomas Bewley, distributed under Modified BSD License.
 
 clear; syms C L R Vs Vd VoutB ILB IL IC IR Vm Vout s
-eqn1= Vm-Vout==Vd/s; eqn2= Vout==R*IR; eqn3= IC==C*(s*Vout-VoutB); eqn4= Vs/s-Vm==L*(s*IL-ILB);
-eqn5= IL==IC+IR; 
+eqn1= Vm-Vout==Vd/s; eqn2= Vout==R*IR; eqn3= IC==C*(s*Vout-VoutB);
+eqn4= Vs/s-Vm==L*(s*IL-ILB); eqn5= IL==IC+IR; 
 SOL=solve(eqn1,eqn2,eqn3,eqn4,eqn5,IL,IC,IR,Vm,Vout); Vout=SOL.Vout, IL=SOL.IL
 pause, disp(' ')
 
@@ -14,15 +14,15 @@ eqn1= b2==B2+B1; eqn2= b1==B2*2*sigma+B1*sigma+B0*omegad; eqn3= b0==B2*(sigma^2+
 SOL=solve(eqn1,eqn2,eqn3,B2,B1,B0); B2=SOL.B2, B1=SOL.B1, B0=SOL.B0
 pause, disp(' ')
 
-clear; syms R L C Vs Vd omegad sigma D f tBmtA tCmtB VoutA VoutB ILA ILB
-omegad=sqrt(1/(L*C)-1/(4*C^2*R^2)); sigma=1/(2*C*R); tBmtA=D/f; tCmtB=(1-D)/f;
-B2=Vs-Vd;      B1=VoutB-(Vs-Vd);   B0=(ILB/C - VoutB*sigma)/omegad - B2*sigma/omegad;
-C2=(Vs-Vd)/R;  C1=ILB-(Vs-Vd)/R;   C0=((Vs - Vd - VoutB)/L + ILB/(C*R) - ILB*sigma)/omegad - C2;
-eqn1= VoutB==VoutA*exp(-(tBmtA)/(R*C));
-eqn2= ILB  ==Vs*(tBmtA)/L + ILA;
-eqn3= VoutA==B2+B1*exp(-sigma*(tCmtB))*cos(omegad*(tCmtB))+B0*exp(-sigma*(tCmtB))*sin(omegad*(tCmtB));
-eqn4= ILA  ==C2+C1*exp(-sigma*(tCmtB))*cos(omegad*(tCmtB))+C0*exp(-sigma*(tCmtB))*sin(omegad*(tCmtB));
-SOL=solve(eqn1,eqn2,eqn3,eqn4,ILB,VoutB,ILA,VoutA); VoutA=SOL.VoutA, VoutB=SOL.VoutB, ILA=SOL.ILA, ILB=SOL.ILB
+clear; syms R L C Vs Vd omegad sigma D f hA hB VoutA VoutB ILA ILB
+omegad=sqrt(1/(L*C)-1/(4*C^2*R^2)); sigma=1/(2*C*R); hA=D/f; hB=(1-D)/f;
+B2=Vs-Vd;     B1=VoutB-(Vs-Vd); B0=(ILB/C-VoutB*sigma)/omegad-B2*sigma/omegad;
+C2=(Vs-Vd)/R; C1=ILB-(Vs-Vd)/R; C0=((Vs-Vd-VoutB)/L+ILB/(C*R)-ILB*sigma)/omegad-C2;
+eqn1= VoutB==VoutA*exp(-hA/(R*C));   eqn2= ILB==Vs*hA/L + ILA;
+eqn3= VoutA==B2+B1*exp(-sigma*hB)*cos(omegad*hB)+B0*exp(-sigma*hB)*sin(omegad*hB);
+eqn4= ILA  ==C2+C1*exp(-sigma*hB)*cos(omegad*hB)+C0*exp(-sigma*hB)*sin(omegad*hB);
+SOL=solve(eqn1,eqn2,eqn3,eqn4,ILB,VoutB,ILA,VoutA);
+VoutA=SOL.VoutA, VoutB=SOL.VoutB, ILA=SOL.ILA, ILB=SOL.ILB
 pause, disp(' ')
 
 Vs=5; Vd=0.5; L=10e-6; C=4.7e-6; R=250; f=1.6e6; D=7/12; % try also D=0.60086;
