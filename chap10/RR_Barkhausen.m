17e13c48b0d16d9ba9e5ea41d8cc61f046862f32
Thomas Bewley <bewley@eng.ucsd.edu>
2021-09-16 15:41:45 -0700

    colpitts codes

--- chap10/RR_Barkhausen.m
+++ chap10/RR_Barkhausen.m
@@ -1,8 +1,8 @@
-function [a3,a4,a5]=RR_Barkhausen(C1,C2,L3,R4,R5,R6,R7)
+function [a4,a3,a5]=RR_Barkhausen(C1,C2,L3,R4,R5,R6,R7)
 % function [a3,a4,a5]=RR_Barkhausen(C1,C2,L3,R4,R5,R6,R7)
-% Computes a3, a4, a5 (see text) of an opamp drivin Colpitts oscillator.
+% Computes {a3,a4,a5} (see text) of an op amp drivin Colpitts oscillator.
 % The Barkhausen condition for sustained oscillations is given by a4<0.
-% EXAMPLE CALL: [a3,a4,a5]=RR_Barkhausen(1e-6,1e-6,1e-3,1,1000,1325,100)
+% EXAMPLE CALL: a4=RR_Barkhausen(1e-6,1e-6,1e-3,1,1000,1325,100)
 % Renaissance Robotics codebase, Chapter 10, https://github.com/tbewley/RR
 % Copyright 2021 by Thomas Bewley, distributed under Modified BSD License.
 
@@ -10,10 +10,5 @@ a2=(C1*C2*R4*R5*R7 + C1*L3*R7 + C2*L3*R5)/(C1*C2*L3*R5*R7);
 a1=(L3 + C2*R4*R5 + C1*R4*R7 + C1*R5*R7 + C2*R5*R7)/(C1*C2*L3*R5*R7);
 a0=(R4+R5+R6+R7)/(C1*C2*L3*R5*R7);
 q=(3*a1-a2^2)/3; r=(2*a2^3-9*a2*a1+27*a0)/27; d=r^2/4+q^3/27;
-a3=a2/3-nthroot(-r/2+sqrt(d),3)-nthroot(-r/2-sqrt(d),3), a4=a2-a3, a5=a0/a3;
-res=a4*a3+a5-a1;  % (check: res should work out to be zero)
-
-% C1=1e-6;  C2=1e-6;  L3=1e-3;  R4=1; R5=1000;  R6=1325;  R7=100;
-% C1=1e-6;  C2=1e-6;  L3=1e-3;  R4=1; R5=1000;  R6=1325;  R7=100;
-% C1=1e-6;  C2=1e-6;  L3=10e-3; R4=1; R5=1000;  R6=1243;  R7=100;
-% C1=10e-6; C2=10e-6; L3=1e-3;  R4=1; R5=1000;  R6=1325;  R7=100;
+a3=a2/3-nthroot(-r/2+sqrt(d),3)-nthroot(-r/2-sqrt(d),3); a4=a2-a3; a5=a0/a3;
+res=a4*a3+a5-a1  % (uncomment to check: res should work out to be near zero)
--- chap10/RR_Colpitts.m
+++ chap10/RR_Colpitts.m
@@ -11,12 +11,3 @@ eqn3=  I4+I7==I1;  eqn7= I2==-C2*(s*Va-Va0);      eqn11= -Vs==R6*I6;
 eqn4=  I5==I6;     eqn8= Va-Vb==L3*(s*I3-I30);    eqn12= Vs-Vout==R7*I7;
 SOL=solve(eqn1,eqn2,eqn3,eqn4,eqn5,eqn6,eqn7,eqn8,eqn9,eqn10,eqn11,eqn12,...
           I1,I2,I3,I4,I5,I6,I7,I8,Va,Vb,Vs,Vout);  SOL.Vout
-pause;
-
-C1=1e-6;  C2=1e-6;  L3=1e-3;  R4=1; R5=1000;  R6=1325;  R7=100;
-a2=(C1*C2*R4*R5*R7 + C1*L3*R7 + C2*L3*R5)/(C1*C2*L3*R5*R7);
-a1=(L3 + C2*R4*R5 + C1*R4*R7 + C1*R5*R7 + C2*R5*R7)/(C1*C2*L3*R5*R7);
-a0=(R4+R5+R6+R7)/(C1*C2*L3*R5*R7);
-q=(3*a1-a2^2)/3; r=(2*a2^3-9*a2*a1+27*a0)/27; d=r^2/4+q^3/27;
-a3=a2/3-nthroot(-r/2+sqrt(d),3)-nthroot(-r/2-sqrt(d),3), a4=a2-a3, a5=a0/a3;
-res=a4*a3+a5-a1;  % (check: res should work out to be zero)
