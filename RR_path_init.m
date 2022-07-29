7e36bbd81abd2d2d48cf415e609f869e73615996
Thomas Bewley <bewley@eng.ucsd.edu>
2022-07-29 12:44:16 -0700

    tweaks

--- RR_path_init.m
+++ RR_path_init.m
@@ -1,19 +1,30 @@
 % script RR_path_init
 % Initialize the path environment for using the Renaissance Robotics (RR) codebase.
 % You can check your path in Matlab at any time by typing "path".
-% PRO TIP: call this init code automatically when firing up Matlab or Octave by appending
-% to the startup.m file in your Matlab or Octave installation as follows:
-%   /bin/zsh
-%     export RRbase="~/RR/"
-%     echo "cd" $RRbase"; RR_path_init" >> ~/Documents/MATLAB/startup.m
-%   exit
-% IMPORTANT: modify the definition of RRbase in the above zsh commands as needed.
+%
+% TIP: call the RR and NR init codes automatically when firing up Matlab on your computer
+% by appending calls to RR_path_init and NR_path_init in the startup.m file of your userpath directory.
+% Matlab provides guidance on this subject here:  https://www.mathworks.com/help/matlab/ref/startup.html
+%
+% In short, first type the command 'userpath' in Matlab, which will return the name of a directory.
+% WITHIN THAT DIRECTORY (important!), edit the file startup.m (or, create a file of this name
+% if one doesn't already exist); this file should contain (at least) the following two lines:
+%    cd '/Users/bewley/RR'; RR_path_init
+%    cd '/Users/bewley/NR'; NR_path_init
+% NOTE: replace the directory names in single quotes above with the full paths to the locations
+% that you have installed the RR and NR codebases on your computer.  Note that forward slashes (/),
+% as shown above, are used on Macs, whereas backslashes (\) are used in Windows; on a Windows machine,
+% the full path to one of these directories might look something like 'C:\Users\bewley\RR'
+% Note you can also put other commonly needed Matlab initialization commands in your startup.m file.
+%
 % Renaissance Robotics codebase, https://github.com/tbewley/RR
-% Copyright 2021 by Thomas Bewley, distributed under BSD 3-Clause License.
+% Copyright 2022 by Thomas Bewley, distributed under BSD 3-Clause License.
 
-format compact, close all, cd ~
-% IMPORTANT: modify the definition of RRbase below as needed:
-RRbase='~/RR/';
+format compact, close all
+% IMPORTANT: modify the definition of RRbase below (inside the single quotes) to indicate the
+% full path to the RR codebase on your computer.  Note that forward slashes (/),
+% as shown below, are used on Macs, whereas backslashes (\) are used in Windows.
+RRbase='/Users/bewley/RR';
 addpath(strcat(RRbase,'/chap01'),strcat(RRbase,'/chap02'),strcat(RRbase,'/chap03'), ...
         strcat(RRbase,'/chap04'),strcat(RRbase,'/chap05'),strcat(RRbase,'/chap06'), ...
         strcat(RRbase,'/chap07'),strcat(RRbase,'/chap08'),strcat(RRbase,'/chap09'), ...
@@ -27,4 +38,3 @@ disp("           "+RRbase)
 disp("in sync with the base branch of the Renaissance Robotics codebase repository at")
 disp("           https://github.com/tbewley/RR")
 disp("See section 2.6 of the Renaissance Robotics text for further info."+newline)
-clear RRbase
--- chapAB/RR_Bezout.m
+++ chapAB/RR_Bezout.m
@@ -7,12 +7,12 @@ function [g,xg,yg] = RR_Bezout(a,b)
 %          xg,yg = vectors of coefficients of output polynomials satisfying the Bezout identity a*xg+b*yg=g   
 % TEST:    b=[1 -1 -2], a=[1 2 -13 -14 24]
 %          [g,xg,yg] = RR_Bezout(a,b)
-%          testg=NR_PolyAdd(NR_PolyConv(a,xg),NR_PolyConv(b,yg)); residualg=norm(NR_PolyAdd(g,-testg))
+%          testg=RR_PolyAdd(RR_PolyConv(a,xg),RR_PolyConv(b,yg)); residualg=norm(RR_PolyAdd(g,-testg))
 % Numerical Renaissance codebase, Appendix B, https://github.com/tbewley/NR
 % Copyright 2022 by Thomas Bewley, distributed under BSD 3-Clause License. 
 
 [g,q,n] = RR_GCF(a,b); 
-x=0; y=1; for j=n-1:-1:1                             % Using the q{i} from the GCF call, compute {xg,yg}
-  t=x; x=y; y=RR_PolyAdd(t,-RR_PolyConv(q{j},y));    % via the Extended Euclidean algorithm
+x=0; y=1; for j=n-1:-1:1                           % Using the q{i} from the GCF call, compute {xg,yg}
+  t=x; x=y; y=RR_PolyAdd(t,-RR_PolyConv(q{j},y));  % via the Extended Euclidean algorithm
 end, yg=y(find(y,1):end); xg=x(find(x,1):end);
 end % function RR_Bezout
