847686d1bd45f971b1c303c4eebccbdb59c394ec
Thomas Bewley <bewley@eng.ucsd.edu>
2022-08-24 08:24:21 -0700

    RR_tf improvements

--- chapAA/RR_Poly.m
+++ chapAA/RR_Poly.m
@@ -1,12 +1,12 @@
-% classdef RR_Poly
+% classdef RR_poly
 %
 % This class defines a set of operations on row vectors, each interpreted as the 
 % coefficients of a polynomial, written in descending orrder.  For example,
 % b=[1 2 0 4] in this class is interpreted as the polynomial b(s)=s^3+2*s^2+4.
 % DEFINITION:
-%   a=RR_Poly(c)          defines an RR_Poly object from a coefficient vector c
-%   a=RR_Poly(r,'roots')  defines an RR_Poly object from a vector of roots r
-%   Note that any RR_Poly object b has two fields, b.poly and b.n
+%   a=RR_poly(c)          defines an RR_poly object from a coefficient vector c
+%   a=RR_poly(r,'roots')  defines an RR_poly object from a vector of roots r
+%   Note that any RR_poly object b has two fields, b.poly and b.n
 % STANDARD OPERATIONS (overloading the +, -, *, ./, and ^ operators):
 %   plus:     a+b  gives the sum of two polynomials
 %   minus:    b-a  gives the difference of two polynomials
@@ -15,50 +15,50 @@
 %   mpower:   a^n  gives the n'th power of a polynomial
 % ADDITIONAL OPERATIONS:
 %   n = norm(b,option)         Gives the norm of b.poly [see: "help norm" - option=2 if omitted]
-%   r = roots(b)               Gives a vector of roots r from a RR_Poly object b
+%   r = roots(b)               Gives a vector of roots r from a RR_poly object b
 %   z = eval(b,s)              Evaluates b(s) for some (real or complex) scalar s
 %   d = diff(p,m)              Computes the m'th derivative of the polynomial p
-% SOME TESTS:  [Try them!!]
-%   a=RR_Poly([1 2 3]), b=RR_Poly([1 2 3 4 5 6])          % Define a couple of test polynomials
+% SOME TESTS:  [Try them! Change them!]
+%   a=RR_poly([1 2 3]), b=RR_poly([1 2 3 4 5 6])          % Define a couple of test polynomials
 %   sum=a+b, diff=b-a, product=a*b, q=b./a, [q,rem]=b./a  % (self explanatory)
 %   check=(a*q+rem)-b, check_norm=norm(check)             % note: check should be the zero polynomial
-%   r=[-3 -1 1 3], b=RR_Poly(r,'roots'), r1=roots(b)      % note: r and r1 should match (change r!)
+%   r=[-3 -1 1 3], b=RR_poly(r,'roots'), r1=roots(b)      % note: r and r1 should match
 %   check_norm=norm(sort(r)-r1)                           % norm should be zero
 %   s1=0, z1=evaluate(b,s1), s2=3, z2=evaluate(b,s2)      % note: z1 should be nonzero, z2 should be zero
 %   for m=0:d = diff(p,m)
 % Renaissance Robotics codebase, Appendix A, https://github.com/tbewley/RR
 % Copyright 2022 by Thomas Bewley and Muhan Zhou, distributed under BSD 3-Clause License.
 
-classdef RR_Poly < matlab.mixin.CustomDisplay
-    properties  % Each RR_Poly object consists of the following two fields:
+classdef RR_poly < matlab.mixin.CustomDisplay
+    properties  % Each RR_poly object consists of the following two fields:
         poly    % The polynomial coefficients themselves, just an ordinary row vector
         n       % The order of this polynomial.  Note that poly has n+1 elements
     end
     methods
-        function obj = RR_Poly(c,flag)       % a=RR_Poly creates an RR_Poly object obj.
+        function obj = RR_poly(c,flag)       % a=RR_poly creates an RR_poly object obj.
             if nargin==1                     % one argument: create obj for c = vector of coefficients            
                  index=find(abs(c(1:end-1))>1e-10,1);     % First trim off any leading zeros!
                  if isempty(index), index=length(c); end 
                  obj.poly = c(index:end);
                  obj.n    = length(obj.poly)-1; 
             else                             % two arguments: create obj for c = vector of roots
-                 obj=RR_Poly([1]); index=1;  % note: flag can be anything!  recommend flag='roots'...  :)
-                 for k=1:length(c), obj=obj*RR_Poly([1 -c(k)]); end % (the resulting p in this case is monic)
+                 obj=RR_poly([1]); index=1;  % note: flag can be anything!  recommend flag='roots'...  :)
+                 for k=1:length(c), obj=obj*RR_poly([1 -c(k)]); end % (the resulting p in this case is monic)
             end 
         end
         function sum = plus(a,b)          % Defines a+b
-            if ~isa(a,'RR_Poly'), a=RR_Poly(a); end,  if ~isa(b,'RR_Poly'), b=RR_Poly(b); end
-            sum = RR_Poly([zeros(1,b.n-a.n) a.poly]+[zeros(1,a.n-b.n) b.poly]);
+            if ~isa(a,'RR_poly'), a=RR_poly(a); end,  if ~isa(b,'RR_poly'), b=RR_poly(b); end
+            sum = RR_poly([zeros(1,b.n-a.n) a.poly]+[zeros(1,a.n-b.n) b.poly]);
         end
         function diff = minus(a,b)        % Defines a-b
-            if ~isa(a,'RR_Poly'), a=RR_Poly(a); end,  if ~isa(b,'RR_Poly'), b=RR_Poly(b); end
-            diff = RR_Poly([zeros(1,b.n-a.n) a.poly]-[zeros(1,a.n-b.n) b.poly]);
+            if ~isa(a,'RR_poly'), a=RR_poly(a); end,  if ~isa(b,'RR_poly'), b=RR_poly(b); end
+            diff = RR_poly([zeros(1,b.n-a.n) a.poly]-[zeros(1,a.n-b.n) b.poly]);
         end    
         function prod = mtimes(a,b)       % Defines a*b
-            if ~isa(a,'RR_Poly'), a=RR_Poly(a); end,  if ~isa(b,'RR_Poly'), b=RR_Poly(b); end
+            if ~isa(a,'RR_poly'), a=RR_poly(a); end,  if ~isa(b,'RR_poly'), b=RR_poly(b); end
             p=zeros(1,b.n+a.n+1);
             for k=0:b.n; p=p+[zeros(1,b.n-k) b.poly(b.n+1-k)*a.poly zeros(1,k)]; end
-            prod=RR_Poly(p);
+            prod=RR_poly(p);
         end
         function [quo,rem] = rdivide(b,a) % Defines [quo,rem]=b./a
             bp=b.poly, ap=a.poly          % <-- just a couple of shorthands to simplify notation
@@ -68,16 +68,16 @@ classdef RR_Poly < matlab.mixin.CustomDisplay
                 dp(j)=bp(1)/ap(1); bp(1:a.n+1)=bp(1:a.n+1)-dp(j)*ap; bp=bp(2:end);
               end
             end
-            quo=RR_Poly(dp); rem=RR_Poly(bp); 
+            quo=RR_poly(dp); rem=RR_poly(bp); 
         end
         function pow = mpower(a,n)        % Defines a^n
-             if n==0, pow=RR_Poly([1]); else, pow=a; for i=2:n, pow=pow*a; end, end
+             if n==0, pow=RR_poly([1]); else, pow=a; for i=2:n, pow=pow*a; end, end
         end
-        function n = norm(a,option)       % Defines n=norm(a,option), where a is an RR_Poly object
+        function n = norm(a,option)       % Defines n=norm(a,option), where a is an RR_poly object
             if nargin<2, option=2; end    % Second argument is optional [see "help norm"]
             n = norm(a.poly,option);
         end
-        function r = roots(p)             % Defines r=roots(p), where p is an RR_Poly object
+        function r = roots(p)             % Defines r=roots(p), where p is an RR_poly object
             r=sort(roots(p.poly))';
         end
         function z = evaluate(a, s)
@@ -86,7 +86,7 @@ classdef RR_Poly < matlab.mixin.CustomDisplay
         function p = diff(p,m)            % Computes the m'th derivative of the polynomial p
             if nargin<2, m=1; end
             p.poly=[p.n:-1:1].*p.poly(1:p.n); p.n=length(p.poly)-1;
-            if p.n<0, p=RR_Poly(0); end
+            if p.n<0, p=RR_poly(0); end
             if m>1, p=diff(p,m-1); end
         end
     end
--- chapAA/RR_tf.m
+++ chapAA/RR_tf.m
@@ -1,18 +1,20 @@
 % classdef RR_tf
 %
 % This class defines a set of operations on transfer functions, each given by a pair of 
-% numerator and denominator polynomials of the RR_Poly class.
+% numerator and denominator polynomials of the RR_poly class.
 % DEFINITION:
-%   G=RR_tf(num,den)    defines an RR_tf object G from numerator and denominator polynomials
-%   G=RR_tf(z,p,K)      defines an RR_tf object G from vectors of zeros and poles, z and p, and the gain K
-%   Note that any RR_tf object G has two RR_Poly fields, G.num and G.den
-% STANDARD OPERATIONS (overloading the +, -, *, ./, and ^ operators):
+%   G=RR_tf(num)      defines an RR_tf object G from a numerator polynomial, setting denominator=1
+%   G=RR_tf(num,den)  2 arguments defines an RR_tf object from numerator and denominator polynomials
+%   G=RR_tf(z,p,K)    3 arguments defines an RR_tf object from vectors of zeros and poles, z and p, and the gain K
+%   Note that any RR_tf object G has two RR_poly fields, G.num and G.den
+% STANDARD OPERATIONS (overloading the +, -, *, ./ operators):
 %   plus:     G1+G2  gives the sum of two transfer functions        (or, a transfer functions and a scalar)
 %   minus:    G1-G2  gives the difference of two transfer functions (or, a transfer functions and a scalar)
 %   mtimes:   G1*G2  gives the product of two transfer functions    (or, a transfer functions and a scalar)
-%   rdivide:  T=G1./G2 divides two transfer functions
-% SOME TESTS:  [Try them!!]
-%   G=RR_tf([1 10],[1 100]), D=RR_tf([1 2],[4 5])          % Define a couple of test transfer functions
+%   rdivide:  G1./G2 divides two transfer functions
+% SOME TESTS:  [Try them! Change them!]
+%   G=RR_tf([1.1 10 110],[1 10 100],1), D=RR_tf([1 2],[4 5])       % Define a couple of test transfer functions
+%   T=G*D./(1+G*D)
 % Renaissance Robotics codebase, Appendix A, https://github.com/tbewley/RR
 % Copyright 2022 by Thomas Bewley, distributed under BSD 3-Clause License.
 
@@ -20,20 +22,31 @@ classdef RR_tf < matlab.mixin.CustomDisplay
     properties
     	num
     	den
+        z
+        p
+        K
     end
     methods
     	function obj = RR_tf(a,b,c)
     		switch nargin
     			case 1  % 1 argument: define RR_tf object from polynomial a
-    				if ~isa(a,'RR_Poly'), a=RR_Poly(a); end, obj = RR_tf(a,1);
+    				if ~isa(a,'RR_poly'), a=RR_poly(a); end, obj = RR_tf(a,1);
     			case 2 	% 2 arguments: define RR_tf object from numerator a and denominator b
-     				if  isa(a,'RR_Poly'), obj.num=a; else, obj.num=RR_Poly(a); end
-   					if  isa(b,'RR_Poly'), obj.den=b; else, obj.den=RR_Poly(b); end
-   					t=1/obj.den.poly(1); obj.den=obj.den*t; obj.num=obj.num*t;  % Make den monic
+     				if  isa(a,'RR_poly'), obj.num=a; else, obj.num=RR_poly(a); end
+   					if  isa(b,'RR_poly'), obj.den=b; else, obj.den=RR_poly(b); end
+   					t=1/obj.den.poly(1); obj.den=obj.den*t; obj.num=obj.num*t;  % Make denominator monic
+                    obj.z=roots(obj.num); obj.p=roots(obj.den); obj.K=obj.num.poly(1); 
    				case 3	% 3 arguments: define RR_tf object from vectors of zeros and poles, a and b, and gain c
-    	    		obj.num=c*RR_Poly(a,'roots'); obj.den=RR_Poly(b,'roots');
+                    obj.z=a; obj.p=b; obj.K=c;
+    	    		obj.num=c*RR_poly(a,'roots'); obj.den=RR_poly(b,'roots');
     	    end
-     	    if obj.num.poly==0, obj.den=RR_Poly(1); end
+     	    if obj.num.poly==0, obj.den=RR_poly(1); end             % Simplify the zero transfer function
+            if obj.num.n>0 & obj.den.n>0, for i=1:obj.num.n     
+                j=find(abs(obj.z(i)-obj.p)<1e-8,1);
+                if ~isempty(j); j=j(1); fprintf('Performing pole/zero cancellation at s=%f\n',obj.z(i))
+                        obj.z=obj.z([1:i-1,i+1:obj.num.n]);
+                        obj.p=obj.p([1:j-1,j+1:obj.den.n]); obj=RR_tf(obj.z,obj.p,obj.K); break, end
+            end, end
     	end
     	function sum = plus(G1,G2)          % Defines G1+G2
             if ~isa(G1,'RR_tf'), G1=RR_tf(G1); end,  if ~isa(G2,'RR_tf'), G2=RR_tf(G2); end
@@ -47,13 +60,19 @@ classdef RR_tf < matlab.mixin.CustomDisplay
             if ~isa(G1,'RR_tf'), G1=RR_tf(G1); end,  if ~isa(G2,'RR_tf'), G2=RR_tf(G2); end
             prod = RR_tf(G1.num*G2.num,G1.den*G2.den);
         end
+        function quo = rdivide(G1,G2)        % Defines G1/G2
+            if ~isa(G1,'RR_tf'), G1=RR_tf(G1); end,  if ~isa(G2,'RR_tf'), G2=RR_tf(G2); end
+            quo=RR_tf(G1.num*G2.den,G1.den*G2.num);
+        end
     end
     methods(Access = protected)
         function displayScalarObject(obj)
-            fprintf(getHeader(obj)),
+            fprintf(getHeader(obj))
             fprintf('num:'), disp(obj.num.poly)
             fprintf('den:'), disp(obj.den.poly)
-            fprintf('  m: %d,  n: %d,  n_r: %d\n', obj.num.n, obj.den.n, obj.den.n-obj.num.n )
+            fprintf('  m: %d,  n: %d,  n_r=n-m: %d,  K: %f\n', obj.num.n, obj.den.n, obj.den.n-obj.num.n, obj.K)
+            fprintf('  z:'), disp(obj.z)
+            fprintf('  p:'), disp(obj.p)
         end
     end
 end
