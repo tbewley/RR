3dfa47bc80c24fa4602e67b3a01746923ab6ae46
Thomas Bewley <bewley@eng.ucsd.edu>
2022-09-14 12:19:49 -0700

    passive filters

--- chap10/RR_Amplifier_Class_A.m
+++ chap10/RR_Amplifier_Class_A.m
@@ -6,7 +6,7 @@
 clear, syms s V_in V_s alpha C0 R1 R2 R3 R4 V_B
 
 % Part A: Solve for V_B as a function of V_in and V_s.
-% x={I_0,I_1,I_2,V_B}   <-- unknown vector
+% x={I_0,I_1,I_2,V_B}   <-- unknown vector (sets I_B=0)
 A  =[ 1   0   0  C0*s;
       0  R1   0   1;
       0   0  R2  -1;
@@ -15,7 +15,7 @@ b  =[ C0*s*V_in; V_s; 0; 0];
 x=A\b; simplify(x(4))
 
 % Part B: Solve for V_out as a function of V_B and V_s.
-% x={I_C,I_E,V_out,V_E}  <-- unknown vector
+% x={I_C,I_E,V_C,V_E,I_5,V_out}  <-- unknown vector
 A  =[-1 alpha  0   0;
       0   0    0   1;
      R3   0    1   0;
