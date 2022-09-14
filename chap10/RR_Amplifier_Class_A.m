6aeebb394f1159014314a246ffd5a2f68e4d1f7c
Thomas Bewley <bewley@eng.ucsd.edu>
2022-09-14 16:21:18 -0700

    amplifier tweaks

--- chap10/RR_Amplifier_Class_A.m
+++ chap10/RR_Amplifier_Class_A.m
@@ -5,20 +5,24 @@
 
 clear, syms s V_in V_s alpha C0 R1 R2 R3 R4 V_B
 
-% Part A: Solve for V_B as a function of V_in and V_s.
-% x={I_0,I_1,I_2,V_B}   <-- unknown vector (sets I_B=0)
-A  =[ 1   0   0  C0*s;
-      0  R1   0   1;
-      0   0  R2  -1;
-      1   1  -1   0];
-b  =[ C0*s*V_in; V_s; 0; 0];
-x=A\b; simplify(x(4))
+% Part A: Solve for V_B as a function of V_in and {V_s,C0,R1,R2}.
+% x={I_0,I_1,I_2,V_B}  <-- unknown vector (assumes I_B is negligible)
+A  =[ 1   1  -1   0;    % I_0 + I_1 - I_2 \approx 0
+      1   0   0  C0*s;  % I_0 + C0*s*V_B = C*s*V_in
+      0  R1   0   1;    % R1*I_1 + V_B = V_s
+      0   0  R2  -1];   % R2*I_2 - V_B = 0
+b  =[ 0; C*s*V_in; V_s; 0];
+x=A\b; V_B=simplify(x(4))
 
-% Part B: Solve for V_out as a function of V_B and V_s.
-% x={I_C,I_E,V_C,V_E,I_5,V_out}  <-- unknown vector
-A  =[-1 alpha  0   0;
+% Part B: Solve for V_out as a function of V_B and {alpha,V_d,V_s,R3,R4,C5}
+% x={I_C,I_B,I_E,V_C,V_B,V_E,I_3,I_5,V_out}  <-- unknown vector
+A  =[ 1   1  -1   0   0   0   0   0   0; % I_B+I_C-I_E = 0
+     -1   0 alpha 0   0   0   0   0   0;
+     
+
+
       0   0    0   1;
      R3   0    1   0;
       0  R4    0  -1];
-b  =[ 0; V_B-0.7; V_s; 0];
-x=A\b; simplify(x(3))
+b  =[ 0; V_B-V_d; V_s; 0];
+x=A\b; V_out=simplify(x(3))
--- chap10/RR_Passive_Filters.m
+++ chap10/RR_Passive_Filters.m
@@ -11,33 +11,33 @@ clear, syms s V_i V_s C L R R1 R2
 
 % First-order low-pass RC filter: Solve for V_o as a function of V_i
 % x={I_C,I_R,V_o}   
-A  =[ 1  -1   0;     % I_C-I_R=0
-      1   0 -C*s;    % I_C-C*s*V_o=0
-      0   R   1];    % R*I_R+V_o=V_i
+A  =[ 1  -1   0;     % I_C - I_R = 0
+      1   0 -C*s;    % I_C - C*s*V_o = 0
+      0   R   1];    % R*I_R + V_o = V_i
 b  =[ 0; 0; V_i];
 x=A\b; LPF1=simplify(x(3))
 
 % First-order high-pass RC filter: Solve for V_o as a function of V_i
 % x={I_C,I_R,V_o}   
-A  =[ 1  -1   0;     % I_C-I_R=0
-      1   0  C*s;    % I_C+C*s*V_o=C*s*V_i
-      0   R  -1];    % R*I_R-V_o=0
+A  =[ 1  -1   0;     % I_C - I_R = 0
+      1   0  C*s;    % I_C + C*s*V_o = C*s*V_i
+      0   R  -1];    % R*I_R - V_o = 0
 b  =[ 0; C*s*V_i; 0];
 x=A\b; HPF1=simplify(x(3))
 
 % Voltage-biased first-order high-pass RC filter: Solve for V_o as a function of V_i and V_s
 % x={I_0,I_1,I_2,V_o}
-A  =[ 1   1  -1   0;   % I_C+I_1-I_2=0
-      1   0   0  C*s;  % I_C+C*s*V_o=C*s*V_i
-      0  R1   0   1;   % R1*I_1+V_o=V_s
-      0   0  R2  -1];  % R2*I_2-V_0=0
+A  =[ 1   1  -1   0;   % I_C + I_1 - I_2 = 0
+      1   0   0  C*s;  % I_C + C*s*V_o = C*s*V_i
+      0  R1   0   1;   % R1*I_1 + V_o = V_s
+      0   0  R2  -1];  % R2*I_2 - V_o = 0
 b  =[ 0; C*s*V_i; V_s; 0];
 x=A\b; HPF1biased=simplify(x(4))
 
 % Second-order low-pass LC filter: Solve for V_o as a function of V_i
 % x={I_L,I_C,V_o}
-A  =[ 1  -1   0;    % I_L-I_C=0
-     L*s  0   1;    % L*s*I_L+V_o=V_i
-      0  -1  C*s];  % C*s*V_o - I_C=0
+A  =[ 1  -1   0;    % I_L - I_C = 0
+     L*s  0   1;    % L*s*I_L + V_o = V_i
+      0  -1  C*s];  % C*s*V_o - I_C = 0
 b  =[ 0; V_i; 0];
 x=A\b; LPF2=simplify(x(3))
