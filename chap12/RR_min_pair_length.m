7c45026226d43b1b6267987208a97e03018d31d1
Thomas Bewley <bewley@eng.ucsd.edu>
2022-11-08 11:27:13 -0800

    Update RR_min_pair_length.m

--- chap12/RR_min_pair_length.m
+++ chap12/RR_min_pair_length.m
@@ -3,10 +3,13 @@ function [seq,length]=RR_min_pair_length(D,n)
 % Given an n x n matrix D (note: n must be even) quantifying the distance between
 % any two nodes in a graph, perform an exhaustive search to minimize the sum,
 % over i=1:2:n-1, of the distances between node seq(i) and seq(i+1).
-% Note: this exhaustive search scales terribly with n.  Best to use for n<=16.
+% Note: this exhaustive search scales terribly with n.  Tested up to n=16.
 % TEST:  n=16; D=rand(n,n); D=D+D'; for i=1:n; D(i,i)=0; end, D
 %        tic, [seq,length]=RR_min_pair_length(D,n), toc
 %        check=0; for i=1:2:n-1, check=check+D(seq(i),seq(i+1)); end, check
+% Renaissance Robotics codebase, Chapter 12, https://github.com/tbewley/RR
+% Copyright 2022 by Thomas Bewley, distributed under BSD 3-Clause License. 
+
 length=Inf;  new_seq(1)=1;
 for i=2:n
     new_seq(2)=i;
