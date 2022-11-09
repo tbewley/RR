0bf96c6e77ae349f03f79de4a378bdf597faf197
Thomas Bewley <bewley@eng.ucsd.edu>
2022-11-09 11:57:16 -0800

    perfected pairwise and triplet swaps

--- chap12/RR_min_pair_length_approx.m
+++ chap12/RR_min_pair_length_approx.m
@@ -4,7 +4,7 @@ function [seq,length]=RR_min_pair_length_approx(D,n,tweak)
 % any two nodes in a graph, estimate the minimizer of the sum,
 % over i=1:2:n-1, of the distances between node seq(i) and seq(i+1).
 % Note: this approximate search scales pretty well with n.  Tested up to n=100.
-% TEST:  clear; n=14; D=rand(n,n); D=D+D'; for i=1:n; D(i,i)=0; end
+% TEST:  clear; n=20; D=rand(n,n); D=D+D'; for i=1:n; D(i,i)=0; end
 %        seq=[1:n], stupid=0; for i=1:2:n-1, stupid=stupid+D(i,i+1); end, stupid
 %        tic, [seq,fast]=RR_min_pair_length_approx(D,n), toc
 %        fast_check=0; for i=1:2:n-1, fast_check=fast_check+D(seq(i),seq(i+1)); end, fast_check
@@ -23,8 +23,15 @@ for i=1:2:n-3
     length=length+D(seq(i),seq(i+1));
 end
 seq(n-1:n)=remaining; length=length+D(seq(n-1),seq(n));
-if nargin>2; modified=true;
-  while modified==true, [seq,length,modified]=pairwise_swaps(seq,length,D,n); end
+if nargin>2;
+  while 1,
+    mod=false;
+    [seq,length,mod]=pairwise_swaps(seq,length,D,n);
+    if mod, fprintf('Pairwise swaps reduced length to'), disp(length), continue, end
+    [seq,length,mod]=triplet_swaps(seq,length,D,n);
+    if mod, fprintf('Triplet swaps reduced length to'), disp(length), continue, end
+    break
+  end
 end
 end % function RR_min_pair_length_approx
 %%%%%%%%%%%%%%%%%%%
@@ -35,17 +42,47 @@ end
 [length,i]=max(row_min); row=[1:i-1,i+1:n]; j=row(row_which(i)); new_pair=sort([i j]);
 end % function max_min_pair_length
 %%%%%%%%%%%%%%%%%%%
-function [seq,length,modified]=pairwise_swaps(seq,length,D,n)
-modified=false;
+function [seq,length,mod]=pairwise_swaps(seq,length,D,n)
+mod=false;
 for i=1:2:n-3
   for j=i+2:2:n-1
-     a=D(seq(i),seq(i+1))+D(seq(j  ),seq(j+1));
-     b=D(seq(i),seq(j  ))+D(seq(i+1),seq(j+1));
-     c=D(seq(i),seq(j+1))+D(seq(j  ),seq(i+1));
-     if     b<a & b<c, [seq(i+1),seq(j  )]=RR_Swap(seq(i+1),seq(j  )); modified=true, length=length-a+b;
-     elseif c<a & c<b, [seq(i+1),seq(j+1)]=RR_Swap(seq(i+1),seq(j+1)); modified=true, length=length-a+c;
+     x=D(seq(i),seq(i+1))+D(seq(j  ),seq(j+1));
+     a=D(seq(i),seq(j  ))+D(seq(i+1),seq(j+1));
+     b=D(seq(i),seq(j+1))+D(seq(j  ),seq(i+1));
+     [M,N] = min([a b x]); switch N
+        case 1, [seq(i+1),seq(j  )]=RR_Swap(seq(i+1),seq(j  )); mod=true; length=length-x+a;
+        case 2, [seq(i+1),seq(j+1)]=RR_Swap(seq(i+1),seq(j+1)); mod=true; length=length-x+b;
      end
   end
 end
 end % function pairwise_swaps
+%%%%%%%%%%%%%%%%%%%
+function [seq,length,mod]=triplet_swaps(seq,length,D,n)
+mod=false;
+for i=1:2:n-5
+  for j=i+2:2:n-3
+    for k=j+2:2:n-1
+       x=D(seq(i),seq(i+1)) +D(seq(j  ),seq(j+1)) +D(seq(k  ),seq(k+1));
+       a=D(seq(i),seq(j  )) +D(seq(k  ),seq(j+1)) +D(seq(i+1),seq(k+1));
+       b=D(seq(i),seq(j  )) +D(seq(k+1),seq(j+1)) +D(seq(k  ),seq(i+1));
+       c=D(seq(i),seq(j+1)) +D(seq(j  ),seq(k  )) +D(seq(i+1),seq(k+1));
+       d=D(seq(i),seq(j+1)) +D(seq(j  ),seq(k+1)) +D(seq(k  ),seq(i+1));
+       e=D(seq(i),seq(k  )) +D(seq(i+1),seq(j+1)) +D(seq(j  ),seq(k+1));
+       f=D(seq(i),seq(k+1)) +D(seq(i+1),seq(j+1)) +D(seq(k  ),seq(j  ));
+       g=D(seq(i),seq(k  )) +D(seq(j  ),seq(i+1)) +D(seq(j+1),seq(k+1));
+       h=D(seq(i),seq(k+1)) +D(seq(j  ),seq(i+1)) +D(seq(k  ),seq(j+1));
+       [M,N] = min([a b c d e f g h x]); switch N
+          case 1, [seq(k  ),seq(i+1),seq(j  )]=RR_Permute(seq(i+1),seq(j  ),seq(k  )); mod=true; length=length-x+a;
+          case 2, [seq(k+1),seq(i+1),seq(j  )]=RR_Permute(seq(i+1),seq(j  ),seq(k+1)); mod=true; length=length-x+b;
+          case 3, [seq(k  ),seq(i+1),seq(j+1)]=RR_Permute(seq(i+1),seq(j+1),seq(k  )); mod=true; length=length-x+c;
+          case 4, [seq(k+1),seq(i+1),seq(j+1)]=RR_Permute(seq(i+1),seq(j+1),seq(k+1)); mod=true; length=length-x+d;
+          case 5, [seq(j  ),seq(k  ),seq(i+1)]=RR_Permute(seq(i+1),seq(j  ),seq(k  )); mod=true; length=length-x+e;
+          case 6, [seq(j  ),seq(k+1),seq(i+1)]=RR_Permute(seq(i+1),seq(j  ),seq(k+1)); mod=true; length=length-x+f;
+          case 7, [seq(j+1),seq(k  ),seq(i+1)]=RR_Permute(seq(i+1),seq(j+1),seq(k  )); mod=true; length=length-x+g;
+          case 8, [seq(j+1),seq(k+1),seq(i+1)]=RR_Permute(seq(i+1),seq(j+1),seq(k+1)); mod=true; length=length-x+h;
+       end
+    end
+  end
+end
+end % function triplet_swaps
 %%%%%%%%%%%%%%%%%%%
