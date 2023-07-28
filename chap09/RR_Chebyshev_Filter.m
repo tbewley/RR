e87fc7b23f85201ee92493a96411527fe6ac4993
Thomas Bewley <bewley@eng.ucsd.edu>
2023-07-27 17:14:37 -0700

    Update RR_Chebyshev_Filter.m

--- chap09/RR_Chebyshev_Filter.m
+++ chap09/RR_Chebyshev_Filter.m
@@ -1,9 +1,13 @@
-function [num,den]=RR_Chebyshev_Filter(n,epsilon)
-% function [num,den]=ChebyshevFilter(n,epsilon)
-% Computes an n'th order Chebyshev filter with cutoff frequency omega_c=1 and 
-% ripple in the passband between 1/(1+epsilon^2) and 1 (see Figures 17.20b, 17.21b).
-% See <a href="matlab:NRweb">Numerical Renaissance: simulation, optimization, & control</a>, Section 17.5.3.2.
-% Part of <a href="matlab:help NRC">Numerical Renaissance Codebase 1.0</a>, <a href="matlab:help NRchap17">Chapter 17</a>; please read the <a href="matlab:help NRcopyleft">copyleft</a>.
-% Verify with: <a href="matlab:help ChebyshevFilterTest">ChebyshevFilterTest</a>.
+function F=RR_Chebyshev_Filter(n,epsilon,omegac)
+% function F=RR_Chebyshev_Filter(n,epsilon,omegac)
+% INPUTS:  n=order of filter (see, e.g., Figures 9.20b, 9.21b)
+%          epsilon=ripple in the passband [between 1/(1+epsilon^2) and 1]
+%          omegac=cutoff frequency of filter (OPTIONAL, taken as 1 if omitted)
+% OUTPUT:  F=n'th order Chebyshev filter of type RR_tf
+% EXAMPLE: F=RR_Chebyshev_Filter(4,0.1,0.1), close all, bode(F)
+% Renaissance Robotics codebase, Chapter 9, https://github.com/tbewley/RR
+% Copyright 2023 by Thomas Bewley, distributed under BSD 3-Clause License.
 
-p=i*cos(acos(i/epsilon)/n+[0:n-1]*pi/n); num=1/(epsilon*2^(n-1)); den=real(Poly(p));
+p=i*cos(acos(i/epsilon)/n+[0:n-1]*pi/n); den=RR_poly(p,1)
+if nargin>2, for k=1:n+1, den.poly(k)=real(den.poly(k))/omegac^(n-k-1); end, end
+num=evaluate(den,0); F=RR_tf(num,den)
