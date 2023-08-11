6fe60facc976499a03917ccb4a4931b705df5f4c
Thomas Bewley <bewley@eng.ucsd.edu>
2023-08-10 17:53:17 -0700

    Update RR_Passive_Filters.m

--- chap10/RR_Passive_Filters.m
+++ chap10/RR_Passive_Filters.m
@@ -7,7 +7,7 @@
 % Renaissance Robotics codebase, Chapter 10, https://github.com/tbewley/RR
 % Copyright 2023 by Thomas Bewley, distributed under Modified BSD License.
 
-clear, syms s V_i V_s C L R R1 R2
+clear, close all, syms s V_i V_s C L R R1 R2
 
 % First-order low-pass RC filter: Solve for V_o as a function of V_i
 % x={I_C,I_R,V_o}  <-- unknown vector   
@@ -16,7 +16,7 @@ A  =[ 1  -1   0;     % I_C - I_R = 0
       0   R   1];    % R*I_R + V_o = V_i
 b  =[ 0; 0; V_i];
 x=A\b; LPF1=simplify(x(3))
-omega1=10; F_LPF1=RR_tf([omega1],[1 omega1]); figure(1), RR_bode(F_LPF1)
+omega1=10; F_LPF1=RR_tf([omega1],[1 omega1]); figure(1), RR_bode(F_LPF1), pause
 
 % First-order high-pass RC filter: Solve for V_o as a function of V_i
 % x={I_C,I_R,V_o}  <-- unknown vector  
@@ -25,7 +25,7 @@ A  =[ 1  -1   0;     % I_C - I_R = 0
       0   R  -1];    % R*I_R - V_o = 0
 b  =[ 0; C*s*V_i; 0];
 x=A\b; HPF1=simplify(x(3))
-omega1=10; F_HPF1=RR_tf([1 0],[1 omega1]); figure(2), RR_bode(F_HPF1)
+omega1=10; F_HPF1=RR_tf([1 0],[1 omega1]); figure(2), RR_bode(F_HPF1), pause
 
 % Voltage-biased first-order high-pass RC filter: Solve for V_o as a function of V_i and V_s
 % x={I_0,I_1,I_2,V_o}  <-- unknown vector
@@ -34,7 +34,7 @@ A  =[ 1   1  -1   0;   % I_C + I_1 - I_2 = 0
       0  R1   0   1;   % R1*I_1 + V_o = V_s
       0   0  R2  -1];  % R2*I_2 - V_o = 0
 b  =[ 0; C*s*V_i; V_s; 0];
-x=A\b; HPF1biased=simplify(x(4))
+x=A\b; HPF1biased=simplify(x(4)), pause
 
 % Second-order low-pass LC filter: Solve for V_o as a function of V_i
 % x={I_L,I_C,V_o}  <-- unknown vector
