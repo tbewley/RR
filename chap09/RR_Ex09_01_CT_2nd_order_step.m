242cc7165ec703eb7dc58552ee30aae791c456a6
Thomas Bewley <bewley@eng.ucsd.edu>
2023-10-03 13:27:58 -0700

    2nd order cases debugged

--- chap09/RR_Ex09_01_CT_2nd_order_step.m
+++ chap09/RR_Ex09_01_CT_2nd_order_step.m
@@ -6,7 +6,7 @@
 % Numerical Renaissance codebase, Chapter 9, https://github.com/tbewley/NR
 % Copyright 2022 by Thomas Bewley, distributed under BSD 3-Clause License. 
 
-clear; syms zeta om_n b0;
+clear; syms zeta om_n b0;                % Compute solution analytically
 sig=zeta*om_n; om_d=om_n*sqrt(1-zeta^2);
 pp=-sig+i*om_d; pm=-sig-i*om_d; Y=RR_tf(b0,RR_poly([0,pp,pm],1))
 [p,d,k,n]=RR_partial_fraction_expansion(Y)
@@ -14,9 +14,19 @@ d0=simplify(d(1))
 dc=simplify(d(2)+d(3))
 ds=simplify(i*(d(2)-d(3)))
 
-close all; om_n=1; b0=1; t=0:.01:10; eps=1e-7; min=eps; max=1-eps; N=10
+close all; figure(1); om_n=1; b0=1; t=0:.01:10; eps=1e-7; min=eps; max=1-eps; N=10
 for zeta=min:(max-min)/N:max, zeta
   d0_n=eval(subs(d0)); dc_n=eval(subs(dc)); ds_n=eval(subs(ds));
   sig_n=eval(subs(sig)); om_d_n=eval(subs(om_d));
   y=exp(-sig_n*t).*(dc_n*cos(om_d_n*t)+ds_n*sin(om_d_n*t))+d0_n; plot(t,y); hold on
 end
+
+clear; figure(2); g.T=10; g.linestyle_y='k-'  % Now, just do it all with RR_step!
+om_n=1; b0=1; eps=1e-7; min=eps; max=1-eps; N=10;
+for zeta=min:(max-min)/N:max, zeta
+  for r=0.7:0.1:1.0
+    r, G=RR_tf(b0,[1 2*zeta*om_n om_n^2]); RR_step(G,g); hold on
+  end
+  axis([0 g.T 0 2])
+end
+% Note: the approach using RR_step extends quite easily to other G(s).
--- chap09/RR_Ex09_02_DT_2nd_order_step.m
+++ chap09/RR_Ex09_02_DT_2nd_order_step.m
@@ -5,7 +5,7 @@
 % Numerical Renaissance codebase, Chapter 9, https://github.com/tbewley/NR
 % Copyright 2022 by Thomas Bewley, distributed under BSD 3-Clause License. 
 
-clear; syms pp pm
+clear; syms pp pm          % Compute solution analytically
 b0=(1-pp)*(1-pm)
 Y=RR_tf(RR_poly([b0 0]),RR_poly([pp pm 1],1)); Y.h=1;
 [p,d,k,n]=RR_partial_fraction_expansion(Y)
@@ -35,18 +35,16 @@ for fig=1:2
   axis([0 kmax 0 2])
 end
 
-clear; close all; kmax=60; k=0:1:kmax;
+clear; g.N=60; g.linestyle_y='k-o'  % Now, just do it all with RR_step!
 for fig=3:4
   figure(fig), switch fig
-     case 1, theta=pi/10; disp('theta=pi/10')
-     case 2, theta=pi/5;  disp('theta=pi/5')
+     case 3, theta=pi/10; disp('theta=pi/10'), 
+     case 4, theta=pi/5;  disp('theta=pi/5')
   end
   for r=0.7:0.1:1.0
     r, a0=r^2; a1=-2*r*cos(theta);
-    G=RR_tf(1+a1+a0,[1 a1 a0]); G.h=1;
-    RR_step(G)
-    plot(k,y,'k-o'); hold on
+    G=RR_tf(1+a1+a0,[1 a1 a0]); G.h=1; RR_step(G,g); hold on
   end
-  axis([0 kmax 0 2])
+  axis([0 g.N 0 2])
 end
-
+% Note: the approach using RR_step extends quite easily to other G(z).
