4ca851c3c009a5744225857e15d638983464b850
Thomas Bewley <bewley@eng.ucsd.edu>
2024-01-22 13:06:28 -0800

    tweaks

--- chap02/RR_ln_8.m
+++ chap02/RR_ln_8.m
@@ -1,13 +1,15 @@
 function [out]=RR_ln_8(x)
 % function [out]=RR_ln_8(x)
 % INPUT:  any x>0
-% OUTPUT: ln(x), with 8 digits of precision (or better)
+% OUTPUT: ln(x), with 8 digits of precision or better
 % TEST:   x=(2*randn)^4, a=log(x), b=RR_ln_8(x), residual=norm(a-b)
 %% Renaissance Robotics codebase, Chapter 2, https://github.com/tbewley/RR
 %% Copyright 2024 by Thomas Bewley, distributed under BSD 3-Clause License.
 
-ln2=0.693147180559945;
+ln2=0.69314718055994531;
 x_b=RR_float_to_bin(x); n=bin2dec(x_b(2:12))-1023; r=x/2^n;
 y=(r-1)/(r+1); sum=0; ypower=2/y; y2=y*y;
-for i=1:2:13; ypower=ypower*y2; sum=sum+ypower/i; end
+% note: if speed is an issue, reduce the number of terms in the sum below
+% for a faster, reduced precision approximation
+for i=1:2:15; ypower=ypower*y2; sum=sum+ypower/i; end
 out=n*ln2+sum;
--- chap02/RR_log2_8.m
+++ chap02/RR_log2_8.m
@@ -1,9 +1,9 @@
-function [out]=RR_log10_8(x)
-% function [out]=RR_log10_8(x)
+function [out]=RR_log2_8(x)
+% function [out]=RR_log2_8(x)
 % INPUT:  any x>0
 % OUTPUT: log10(x), with 8 digits of precision or better
-% TEST:   x=(2*randn)^4, a=log10(x), b=RR_log10_8(x), residual=norm(a-b)
+% TEST:   x=(2*randn)^4, a=log2(x), b=RR_log2_8(x), residual=norm(a-b)
 %% Renaissance Robotics codebase, Chapter 2, https://github.com/tbewley/RR
 %% Copyright 2024 by Thomas Bewley, distributed under BSD 3-Clause License.
 
-ln10inv=0.43429448190325183; out=RR_ln_8(x)*ln10inv;
+ln2inv=1.44269504088896341; out=RR_ln_8(x)*ln2inv;
