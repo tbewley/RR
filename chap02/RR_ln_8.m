05a36a14d1502482fe5b66eb99eda54804e196c3
Thomas Bewley <bewley@eng.ucsd.edu>
2024-01-24 07:41:50 -0800

    Update RR_ln_8.m

--- chap02/RR_ln_8.m
+++ chap02/RR_ln_8.m
@@ -8,8 +8,8 @@ function [out]=RR_ln_8(x)
 
 ln2=0.69314718055994531;
 x_b=RR_float_to_bin(x); n=bin2dec(x_b(2:12))-1023; r=x/2^n;
-y=(r-1)/(r+1); sum=0; ypower=2/y; y2=y*y;
+y=(r-1)/(r+1); ys=y*y; num=2*y; sum=num;
 % note: if speed is an issue, reduce the number of terms in the sum below
 % for a faster, reduced precision approximation
-for i=1:2:15; ypower=ypower*y2; sum=sum+ypower/i; end
+for i=3:2:15; num=num*ys; sum=sum+num/i; end % (1 mult, 1 div, 1 add per step)
 out=n*ln2+sum;
