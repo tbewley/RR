62b7d6c908c489e459ace5df71000f1ec0b4dd47
Thomas Bewley <bewley@eng.ucsd.edu>
2024-03-10 19:34:13 -0700

    Update RR_PCG.m

--- Renaissance_Robotics/chap02/RR_PCG.m
+++ Renaissance_Robotics/chap02/RR_PCG.m
@@ -1,53 +1,56 @@
 function x=RR_PCG(thread)
 % PRNG using Melissa O'Neill's Permuted Congruential Generator (PCG), with 64 bit state and 32 bit output
-% for each thread; many many independent threads are possible.  For code simplicity, multiple threads
+% for each thread; many many independent threads are possible. NOTE: for code simplicity, multiple threads
 % must be initialized in numerical order, but can be called in arbitrary order after that.
 % INPUT: thread = which independent thread to pull random number from (optional, thread=1 by default)
-% TEST:  RR_PCG_minimal(1), RR_PCG_minimal(1), RR_PCG_minimal(1)  % Begin thread=1
-%        RR_PCG_minimal(2), RR_PCG_minimal(2), RR_PCG_minimal(2)  % Begin thread=2
-%        RR_PCG_minimal(1), RR_PCG_minimal(1), RR_PCG_minimal(1)  % continue random sequence in thread=1
+% TEST:  RR_PCG(1), RR_PCG(1), RR_PCG(1)  % Begin thread=1
+%        RR_PCG(2), RR_PCG(2), RR_PCG(2)  % Begin thread=2
+%        RR_PCG(1), RR_PCG(1), RR_PCG(1)  % continue random sequence in thread=1
+%        clear RR_PCG, for i=1:100, RR_PCG(i); end  % inspect the random initilization of 100 threads
 %        % The following test recovers the 32bit output (in hex) of Round 1 of O'Neill's pcg32-demo code
-%        clear RR_PCG_minimal; for i=1:6, dec2hex(RR_PCG_minimal(0)), end  
-% Note: if this routine, for this thread, hasn't been run yet in this Matlab session,
-% it initializes the state using the time/date.
+%        clear RR_PCG; for i=1:6, dec2hex(RR_PCG(0)), end  
 % DEPENDENCIES: This Matlab code uses the RR_uint64 class defined by the code at:
 %   https://github.com/tbewley/RR/blob/main/chapAA/RR_uint64.m
 %% Renaissance Repository, https://github.com/tbewley/RR (Renaissance Robotics, Chapter 2)
 %% Efficient PCG algorithm (in C and C++) due to Melissa O'Neill, available at
 %%    https://www.pcg-random.org/download.html
-%% Matlab translation (meant for pedagogical purposes only) by Thomas Bewley.
+%% Matlab translation (meant primarily for pedagogical purposes) by Thomas Bewley.
 
-persistent state inc  % Matlab will hold these variables as "persistent" for this function
+persistent state inc % Matlab will hold these variables as "persistent" for this function
 if nargin==0, thread=1, end, t=max(thread,1);  % note: thread=0 test case makes use of thread=1
 if length(state)==0; state={}; inc={}; end
-if length(state)<t                           % INITIALIZATION OF {state,inc}
-  if thread==0                               % Test case: Initialize state deterministically
-  	disp('Initializing some deterministic values of {state,inc}, for TEST PURPOSES ONLY')
-    old_state=RR_uint64(0x185706B82C2E03F8); % Generates values identical to Round 1 of pcg32-demo.c code
-    inc{1}=RR_uint64(109);                   
-  else                                       % Other cases: Initialize state randomly based on the clock
-    fprintf('Initializing a random value of oldstate, increment for thread=%d\n',t)
-    date=convertTo(datetime,'epochtime','Epoch','1-Jan-2024','TicksPerSecond',1e6);
-    old_state=RR_uint64(bin2dec(fliplr(dec2bin(date,50))))
-    % Initialize increment as convenient odd integer
-    if thread==1; inc{t}=RR_uint64(date); bitset(date,1); % Note inc must be odd.
-    % note a=dec2hex(uint64(1987591058829310733)*2), taking 2*a, c=0 (m=2^63) generator from L’Ecuyer (1999) 
-    else, inc{t}=(inc{t-1}-1)*0x372AAF131886F61A+1; end   % and adding 1 to keep each value of inc odd.
-    inc{t}=increment;
+if length(state)<t                                % INITIALIZATION OF {state,inc}
+  if t>1 & length(state)<t-1, error('RR_PCG threads must be initialized sequentially'), end
+  switch thread
+    case 0, disp('Initializing some deterministic values of {state,inc}, for TEST PURPOSES ONLY')
+      old_state=RR_uint64(0x185706B82C2E03F8); % Generates values identical to Round 1
+      inc{1}   =RR_uint64(109);                % of O'Neill's pcg32-demo.c code
+    case 1, % initialize {state,inc} based on milliseconds since most recent New Years Day (NYD)
+      NYD      =strcat('1-Jan-',num2str(year(datetime))); 
+      old_state=RR_uint64(convertTo(datetime,'epochtime','Epoch',NYD,'TicksPerSecond',1e6))*0x2545F4914F6CDD1D;
+      inc{1}   =RR_uint64(bin2dec(fliplr(dec2bin(old_state.v,50))))*0x372AAF131886F61A;
+      inc{1}.v =bitset(inc{1}.v,1);
+      fprintf('Initializing PCG thread   1 with old_state=%20u and odd increment=%20u\n',old_state.v,inc{1}.v)
+    otherwise
+      % note: take a=dec2hex(uint64(a)) for a=3935559000370003845 and 2685821657736338717 from L’Ecuyer (1999) 
+      old_state = state{t-1} *0x369DEA0F31A53F85+1; % initialize state{t} using state{t-1}
+      inc{t}    =(inc{t-1}-1)*0x2545F4914F6CDD1D+1; % (subtracting/adding 1 keeps each inc{t} odd)
+    fprintf('Initializing PCG thread %3d with old_state=%20u and odd increment=%20u\n',t,old_state.v,inc{t}.v)
   end
 else 
   old_state=state{t};  % Continuing computation in thread t
 end
 
-% Update internal 64-bit state for this thread
+% Update internal 64-bit state of PCG for this thread
 state{t}=old_state*0x5851F42D4C957F2D+inc{t};
+state{t}
 % Then, calculate a 32-bit output using XSH RR method, performing PCG output bit permutations.
 % See section 6.3.1 of https://www.pcg-random.org/pdf/hmc-cs-2014-0905.pdf 
 x   = uint32(bitand(bitsra(bitxor(bitsra(old_state.v,18),old_state.v),27),0x00000000FFFFFFFF));
 rot = uint32(bitsra(old_state.v,59));
 x   = bitor(bitsra(x,rot),bitsll(x,(bitand(bitcmp(rot)+1,31))));
 
-% For comparison to O'Neill's PCG, here are some bitwise operations on integers, in both C and Matlab
+% For comparison to O'Neill's PCG, here are some bitwise operations on integers, in both C and Matlab:
 %  C  Bit Operations   Matlab
 % ------------------------------
 % a>>k  rightshift   bitsra(a,k)
