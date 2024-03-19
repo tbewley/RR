2fdc8bf48691a6b1569346dec7fa6c592e0d3a09
Thomas Bewley <bewley@eng.ucsd.edu>
2024-03-19 15:14:34 -0700

    MCG/LCG/PCG filenames

--- Renaissance_Robotics/chap02/PRNGs/RR_LCG256_test.m
+++ Renaissance_Robotics/chap02/PRNGs/RR_LCG256_test.m
@@ -1,18 +1,20 @@
-% script RR_small_LCG_test.m
+% script RR_LCG256_test.m
+% Script illustrating the behavior of simple PRNGs with LCGs of period m=2^8=256.
 %% Renaissance Repository, https://github.com/tbewley/RR (Renaissance Robotics, Chapter 2)
 %% Copyright 2024 by Thomas Bewley, published under BSD 3-Clause License.
 
 clear, m=256, a(1)=157, c(1)=47, [g,e,astar]=RR_bezout(RR_int(m),RR_int(a));
-astar=mod(int32(astar.v),int32(m)),  cstar=mod(c*(m-astar),m), % pause
+astar=mod(int32(astar.v),int32(m)),  cstar=mod(c*(m-astar),m), pause
 
-x(1)=0; for i=2:m+5, x(i)=mod(a*x(i-1)+c,m); end, x, dec2bin(x(1:9)), % pause
+x(1)=0; for i=2:m+5, x(i)=mod(a*x(i-1)+c,m); end, x, dec2bin(x(1:9)), pause
 
-x(1)=0; for i=2:m+5, x(i)=mod(astar*x(i-1)+cstar,m); end, x, % pause
+x(1)=0; for i=2:m+5, x(i)=mod(astar*x(i-1)+cstar,m); end, x, pause
 
 m=256, a=[157 45 181 125], c=[47 51 47 47],
 for j=1:4; figure(j), clf, axis equal, axis([0 256 0 256]), hold on
   x(1)=0; for i =2:256, x(i)=mod(a(j)*x(i-1)+c(j),m); plot(x(i-1),x(i),'kx'), end
-  name=sprintf('ktuple_clustering%d',j), print('-vector',name,'-depsc'), pause
+  % name=sprintf('ktuple_clustering%d',j), print('-vector',name,'-depsc'),
+  pause
 end, pause
 
 figure(5), clf, axis equal, axis([0 256 0 256]), hold on
@@ -20,6 +22,8 @@ x=0; y=1; plot(x,y,'kx'), fprintf('i=')
 for i=1:50, fprintf('%d ',i)
   for j=1:256, x=mod(a(1)*x+c(1),m); y=mod(a(2)*y+(1+(x==0))*c(2),m); plot(x,y,'kx'), end
   switch i
-    case {1,2,3,4,5,6,7,20,50}, name=sprintf('k_dim_%02d_256',i), print('-vector',name,'-depsc'), pause
+    case {1,2,3,4,5,6,7,20,50}
+      % name=sprintf('k_dim_%02d_256',i), print('-vector',name,'-depsc')
+      pause
   otherwise, pause(0.001); end
 end
--- Renaissance_Robotics/chap02/PRNGs/RR_MCG4294967291.m
+++ Renaissance_Robotics/chap02/PRNGs/RR_MCG4294967291.m
@@ -1,16 +1,16 @@
-function x=RR_LCG32(i_max,reverse)
-% Pseudo Random Number Generator with an LCG with period m=2^16-15=65521.
+function x=RR_MCG4294967291(i_max,reverse)
+% Simple PRNG with an MCG with period m=2^32-5=4294967291
 % Note: if this routine hasn't been run yet in this Matlab session, it
 % initializes the previous state using the fractional seconds of the clock.
 % INPUTS: i_max   (OPTIONAL) number of random numbers to return.  default=1
 %         reverse (OPTIONAL) set to true to run backward, omit to run forward
 % OUTPUT: x
-% TESTs:  RR_LCG32(7), RR_LCG32(6,true)
+% TEST:   RR_MCG4294967291(7), RR_MCG4294967291(6,true)
 %% Renaissance Repository, https://github.com/tbewley/RR (Renaissance Robotics, Chapter 2)
 %% Copyright 2024 by Thomas Bewley, published under BSD 3-Clause License.
 
 persistent XOLD, if nargin<1, i_max=1; end
-m=65521; if nargin<2, a=32236; else, a=17364; end
-if isempty(XOLD), t=second(datetime), XOLD=round((t-floor(t))*m), end
+m=uint64(4294967291); if nargin<2, a=uint64(1815976680); else, a=uint64(279470273); end
+if isempty(XOLD), t=second(datetime), XOLD=uint64(round((t-floor(t))*m)), end
 x=mod(a*XOLD,m);
 for i=2:i_max, x(i)=mod(a*x(i-1),m); end, XOLD=x(end);
