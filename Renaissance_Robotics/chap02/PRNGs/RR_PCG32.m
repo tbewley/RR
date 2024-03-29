d959c41eb62578c23850f25782f7112f00c31834
Thomas Bewley <bewley@eng.ucsd.edu>
2024-03-29 06:34:58 -0700

    debugged skip in RR_PCG32

--- Renaissance_Robotics/chap02/PRNGs/RR_PCG32.m
+++ Renaissance_Robotics/chap02/PRNGs/RR_PCG32.m
@@ -1,65 +1,96 @@
-function x=RR_PCG32(stream,skip)
-% function x=RR_PCG32(stream,skip)
+function [out,X]=RR_PCG32(stream,skip)
+% function out=RR_PCG32(stream,skip)
 % PRNG using Melissa O'Neill's Permuted Congruential Generator PCG32, with 64 bit state and 32 bit output
 % for each stream; zillions of independent streams are possible. NOTE: for code simplicity, multiple streams
 % must be initialized in numerical order, but can be called in arbitrary order after that.
 % Initialization is based on the number of microseconds since midnight on New Years Eve in 2023.
-% INPUT: stream = which independent stream to pull random number from (OPTIONAL, stream=1 by default)
-%        skip = the number of steps to skip forward or backward in stream (OPTIONAL, skip=0 by default)
+% INPUT: stream = which independent stream to pull random number from     (OPTIONAL, stream=1 by default)
+%        skip = the number of steps to skip forward or backward in stream (OPTIONAL, skip=0   by default)
 % TEST:  RR_PCG32(1), RR_PCG32(1), RR_PCG32(1)  % Begin stream 1
 %        RR_PCG32(2), RR_PCG32(2), RR_PCG32(2)  % Begin stream 2
 %        RR_PCG32(1), RR_PCG32(1), RR_PCG32(1)  % continue random sequence in stream 1
-%        RR_PCG32(1,-6)                     % skip backwards 6 steps in PCG stream 1
-%        clear RR_PCG, for i=1:100, RR_PCG32(i); end  % inspect the random initilization of 100 streams
+%        RR_PCG32(1,-6) % skip backwards 6 steps in PCG stream 1 (recovers first random number generated above)
+%        clear RR_PCG32, for i=1:100, RR_PCG32(i); end  % inspect the random initilization of 100 streams
 %        % The following test recovers the 32bit output (in hex) of Round 1 of O'Neill's pcg32-demo code
 %        clear RR_PCG32; for i=1:6, dec2hex(RR_PCG32(0)), end  
 % DEPENDENCIES: This Matlab code depends, internally, on the RR_uint64 class defined by:
 %   https://github.com/tbewley/RR/blob/main/Renaissance_Robotics/chapAA/classes/RR_uint64.m
 % Renaissance Repository, https://github.com/tbewley/RR (Renaissance Robotics, Chapter 2)
 %% Efficient PCG algorithm (in C and C++) due to Melissa O'Neill, available at
-%%    https://www.pcg-random.org/download.html
+%%    https://www.pcg-random.org/download.html 
 %% Matlab translation (meant primarily for pedagogical purposes) by Thomas Bewley.
 
-% TODO: implement skip!
-
-persistent state inc % Matlab will hold these 2 RR_uint64 variables as "persistent" for each stream of RR_PCG
+persistent a astar  % initialize {a,astar} (multipliers) just once (they are the same for each stream)
+persistent x c % hold RR_uint64 variables x (state) and c (increment) as "persistent" for each stream of RR_PCG
 if nargin==0, stream=1, end, s=max(stream,1);  % note: stream=0 test case makes use of stream=1
-if length(state)<s                             % INITIALIZATION OF {state,inc} FOR THIS STREAM
-  if length(state)==0; state={}; inc={}; end
-  if s>1 & length(state)<s-1, error('RR_PCG streams must be initialized sequentially'), end
+if length(x)<s                             % INITIALIZATION OF {state,inc} FOR THIS STREAM
+  if length(x)==0;
+    x={}; inc={}; a=RR_uint64(0x5851F42D4C957F2D) % set up x, inc, a
+              astar=RR_uint64(0xC097EF87329E28A5) % astar was found by solving z*2^m+a*astar=g (with g=1),
+  end                                             % which was done using [g,z,astar] = RR_bezout(RR_uint64(0),a);
+  if s>1 & length(x)<s-1, error('RR_PCG streams must be initialized sequentially'), end
   switch stream
-    case 0, disp('Initializing some deterministic values of {state,inc}, for TEST PURPOSES ONLY')
-      old_state=RR_uint64(0x185706B82C2E03F8); % Generates values identical to Round 1
-      inc{1}   =RR_uint64(109);                % of O'Neill's pcg32-demo.c code
-    case 1, % initialize stream 1 based on the number of microseconds since midnight on 31 Dec 2023
-      old_state=RR_uint64(convertTo(datetime,'epochtime','Epoch','1-Jan-2024','TicksPerSecond',1e6));
-      inc{1}   =RR_uint64(bin2dec(fliplr(dec2bin(old_state.v,50))))*0x372AAF131886F61A;
-      old_state=old_state*0x2545F4914F6CDD1D; inc{1}.v =bitset(inc{1}.v,1);
-      fprintf('Initializing PCG stream   1 with old_state=%20u and odd increment=%20u\n',old_state.v,inc{1}.v)
-    otherwise % initialize inc and state of stream t>1 based on inc and current state of stream t-1
+    case 0, disp('Initializing some deterministic values of {x,c}, for TEST PURPOSES ONLY')
+      x_old = RR_uint64(0x185706B82C2E03F8); % Generates values identical to Round 1
+      c{1}  = RR_uint64(109);                % of O'Neill's pcg32-demo.c code
+    case 1, % initialize stream s=1 based on the number of microseconds since midnight on 31 Dec 2023
+      x_old = RR_uint64(convertTo(datetime,'epochtime','Epoch','1-Jan-2024','TicksPerSecond',1e6));
+      c{1}  = RR_uint64(bin2dec(fliplr(dec2bin(x_old.v,50))))*0x372AAF131886F61A;
+      x_old = x_old*0x2545F4914F6CDD1D; c{1}.v=bitset(c{1}.v,1);
+      fprintf('Initializing PCG stream   1 with x_old=%20u and odd increment=%20u\n',x_old.v,c{1}.v)
+    otherwise % initialize x_old and c of stream s>1 based on x and c of stream s-1
       % note: take a=dec2hex(uint64(a)) for a=3935559000370003845 and 2685821657736338717 from L’Ecuyer (1999) 
-      old_state = state{s-1} *0x369DEA0F31A53F85+1; % initialize state{s} using state{s-1}
-      inc{s}    =(inc{s-1}-1)*0x2545F4914F6CDD1D+1; % (subtracting/adding 1 keeps each inc{t} odd)
-    fprintf('Initializing PCG stream %3d with old_state=%20u and odd increment=%20u\n',s,old_state.v,inc{s}.v)
+      x_old = x{s-1} *0x369DEA0F31A53F85+1;    % initialize x{s} using x{s-1}
+      c{s}  = (c{s-1}-1)*0x2545F4914F6CDD1D+1; % (subtracting/adding 1 keeps each c{s} odd)
+    fprintf('Initializing PCG stream %3d with x_old=%20u and odd increment=%20u\n',s,x_old.v,c{s}.v)
   end
 else 
-  old_state=state{s};  % Continuing computation in stream t
+  x_old=x{s};  % Continuing computation in stream s 
 end
 
-% Update internal 64-bit state of LCG for this stream
-state{s}=old_state*0x5851F42D4C957F2D+inc{s};
-% Then, calculate a 32-bit PCG output using XSH RR method, performing output bit permutations.
-% See section 6.3.1 of https://www.pcg-random.org/pdf/hmc-cs-2014-0905.pdf 
-x   = uint32(bitand(bitsra(bitxor(bitsra(old_state.v,18),old_state.v),27),0x00000000FFFFFFFF));
-rot = uint32(bitsra(old_state.v,59));
-x   = bitor(bitsra(x,rot),bitsll(x,(bitand(bitcmp(rot)+1,31))));
+% Implement skip algorithnm from Forrest Brown (1994) Random number generation with arbitrary strides,
+% as summarized at https://mcnp.lanl.gov/pdf_files/TechReport_2007_LANL_LA-UR-07-07961_Brown.pdf
+% Noting that x_{k_n}=A*x_n+C, we first need to calculate A=mod(a^k,2^m) and C=mod(c*(a^k-1)/(a-1),2^m) 
+% Use astar and cstar instead of a and c if skipping backwards (reduces the number of steps used!)
+if nargin==2 & skip~=0
+   A=RR_uint64(1); C=RR_uint64(0); ib=dec2bin(abs(skip)); imax=length(ib);
+   if skip>0, h=a; f=c{s}; else, h=astar; f=c{s}*(-astar); end, 
+   for i=imax:-1:1, if str2num(ib(i)), A=A*h; C=C*h+f; end, f=f*(h+1); h=h*h; end
+   x_old=A*x_old+C;   % Do the skip 
+end
 
-% For comparison to O'Neill's PCG, here are some bitwise operations on integers, in both C and Matlab:
-%  C  Bit Operations   Matlab
-% ------------------------------
-% a>>k  rightshift   bitsra(a,k)
-% a<<k  leftshift    bitsll(a,k)
-% a&b      AND       bitand(a,b)
-% a^b      XOR       bitxor(a,b)
-% a|b      OR        bitor(a,b)
-% -a     2's comp.   bitcmp(a)+1
+x{s}=a*x_old+c{s};   % Update internal 64-bit state of LCG for this stream
+
+% Then, calculate a 32-bit PCG output using O'Neill's XSH_RR output bit permutations.
+% See section 6.3.1 of https://www.pcg-random.org/pdf/hmc-cs-2014-0905.pdf 
+out = uint32(bitand(bitsra(bitxor(bitsra(x_old.v,18),x_old.v),27),0x00000000FFFFFFFF));
+rot = uint32(bitsra(x_old.v,59));
+out = bitor(bitsra(out,rot),bitsll(out,(bitand(bitcmp(rot)+1,31))));
+
+if nargout==2, X=x{s}; end  % If requested, also return current state
+
+% For comparison to O'Neill's PCG code, here are some bitwise operations on integers, in both C and Matlab:
+% Bit Operation  C      Matlab
+% -------------------------------
+% rightshift    a>>k  bitsra(a,k)
+% leftshift     a<<k  bitsll(a,k)
+%    AND        a&b   bitand(a,b)
+%    XOR        a^b   bitxor(a,b)
+%    OR         a|b   bitor(a,b)
+% 2's comp.     -a    bitcmp(a)+1
+
+% Initializing some deterministic values of {x,c}, for TEST PURPOSES ONLY
+%   3118741472915405573
+%   10030406343644371790
+%   17800363335834976035
+%   16987470693695682196
+%   9440484487994590321
+%   13742400798436595530
+%   17113982732917624431
+%   518186974999665392
+%   10480504684531518621
+
+
+
+
+
