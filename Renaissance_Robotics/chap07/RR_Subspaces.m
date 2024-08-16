282dd141e518f058ad23757dca167e4fb4682eb8
Thomas Bewley <bewley@eng.ucsd.edu>
2024-08-16 17:03:22 -0600

    update RR_Subspaces

--- Renaissance_Robotics/chap07/RR_Subspaces.m
+++ Renaissance_Robotics/chap07/RR_Subspaces.m
@@ -1,4 +1,12 @@
-format short
+% script RR_Subspaces
+% Calculates orthogonal bases for the 4 fundamental subspaces of several interesting matrices A
+% using QR decompositions of A and A'.  The code scales the columns of these basis vectors in a
+% convenient way, so their entries are simple integers (that is, their columns are not normalized).
+% The results that this script produces are helpful for understanding the Strang plot.
+%% Renaissance Repository, https://github.com/tbewley/RR (Renaissance Robotics, Chapter 7)
+%% Copyright 2024 by Thomas Bewley, published under BSD 3-Clause License.
+
+clear, format short
 clc, A=[1 2 3;4 5 6;7 8 0], determinant=det(A), r=rank(A), pause
 Ainv = inv(A), fac=scale_factor(Ainv),  Ainv_times_fac=Ainv*fac, A*Ainv, pause
 
@@ -8,13 +16,13 @@ A, A_times_A=A*A, pause, disp(' ')
 
 clc, A=[2 2 -2;5 1 -3;1 5 -3], [C,L,R,N]=test_3_by_3_matrix(A);
 disp('here is a surprise! (A is nilpotent of degree 3)');
-A, A_times_A=A*A, A_times_A_times_A=A*A*A, pause, disp(' ')
+A, A_times_A=A*A, A_times_A_times_A=A*A*A, pause
 
 clc, A=[1 2;3 4;0 0], [C,L,R,N]=test_rectangular_matrix(A);
-gamma=randi([-10 10]), yL=gamma*L(:,1), Apinv_times_yL=Apinv*yL, pause, disp(' ')
+gamma=randi([-10 10]), yL=gamma*L(:,1), Apinv_times_yL=Apinv*yL, pause
 
 clc, A=[1 2 0;3 4 0], [C,L,R,N]=test_rectangular_matrix(A);
-delta=randi([-10 10]), xN=delta*N(:,1), A_times_xN=A*xN, pause, disp(' ')
+delta=randi([-10 10]), xN=delta*N(:,1), A_times_xN=A*xN, pause
 
 disp('Some pretty nifty stuff, eh?')
 
@@ -42,11 +50,11 @@ function [C,L]=QRcheck(A,r)
 % compute the QR decomposition of A, and rescale/rename the columns of Q
 [Q,R]=qr(A); L=[];
 if sign(Q(1,1)) ~= sign(A(1,1)), Q=-Q; end  % select a natural sign for Q(1,1)
-for i=1:r            % rescale/rename first r columns of Q
-   C(:,i)=Q(:,i);    fac=scale_factor(C(:,i)); C(:,i)=C(:,i)*fac;
+for i=1:r           % rescale/rename first r columns of Q
+   C(:,i)=Q(:,i);   fac=scale_factor(C(:,i)); C(:,i)=C(:,i)*fac;
 end
-for i=1:size(Q,2)-r  % rescale/rename last r columns of Q
-   L(:,i)=Q(:,r+i);  fac=scale_factor(L(:,i)); L(:,i)=L(:,i)*fac;                
+for i=1:size(Q,2)-r % rescale/rename last r columns of Q
+   L(:,i)=Q(:,r+i); fac=scale_factor(L(:,i)); L(:,i)=L(:,i)*fac;                
 end
 end
 
