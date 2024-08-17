5a5be7afadc32e28972d9fbf44d258e6405ed058
Thomas Bewley <bewley@eng.ucsd.edu>
2024-08-16 22:19:58 -0600

    Update RR_Subspaces.m

--- Renaissance_Robotics/chap07/RR_Subspaces.m
+++ Renaissance_Robotics/chap07/RR_Subspaces.m
@@ -10,23 +10,25 @@ clear, format short
 clc, A=[1 2 3;4 5 6;7 8 0], determinant=det(A), r=rank(A), pause
 Ainv = inv(A), fac=scale_factor(Ainv),  Ainv_times_fac=Ainv*fac, A*Ainv, pause
 
-clc, A=[2 -2 -4;-1 3 4;1 -2 -3], [C,L,R,N]=test_3_by_3_matrix(A);
+clc, A=[2 -2 -4;-1 3 4;1 -2 -3], [C,L,R,N,Apinv]=test_3_by_3_matrix(A);
 disp('here is a surprise! (A is idempotent)');
 A, A_times_A=A*A, pause, disp(' ')
 
-clc, A=[2 2 -2;5 1 -3;1 5 -3], [C,L,R,N]=test_3_by_3_matrix(A);
+clc, A=[2 2 -2;5 1 -3;1 5 -3], [C,L,R,N,Apinv]=test_3_by_3_matrix(A);
 disp('here is a surprise! (A is nilpotent of degree 3)');
 A, A_times_A=A*A, A_times_A_times_A=A*A*A, pause
 
-clc, A=[1 2;3 4;0 0], [C,L,R,N]=test_rectangular_matrix(A);
+clc, A=[1 2;3 4;0 0], [C,L,R,N,Apinv]=test_rectangular_matrix(A);
 gamma=randi([-10 10]), yL=gamma*L(:,1), Apinv_times_yL=Apinv*yL, pause
 
-clc, A=[1 2 0;3 4 0], [C,L,R,N]=test_rectangular_matrix(A);
+clc, A=[1 2 0;3 4 0], [C,L,R,N,Apinv]=test_rectangular_matrix(A);
 delta=randi([-10 10]), xN=delta*N(:,1), A_times_xN=A*xN, pause
 
 disp('Some pretty nifty stuff, eh?')
+disp('Note that an alternative method to generate orthogonal bases of {C,L,R,N}')
+disp('is to calculate [U,S,V] = svd(A), as described in the notes.')
 
-function [C,L,R,N]=test_3_by_3_matrix(A)
+function [C,L,R,N,Apinv]=test_3_by_3_matrix(A)
 determinant=det(A), r=rank(A), pause, disp(' ')
 [C,L]=QRcheck(A,r),  pause, disp(' ')
 [R,N]=QRcheck(A',r), pause, disp(' ')
@@ -37,7 +39,7 @@ delta=ran, xN=delta*N(:,1), A_times_xN=A*xN, pause, disp(' ')
 gamma=ran, yL=gamma*L(:,1), Apinv_times_yL=Apinv*yL, pause, disp(' ')
 end
 
-function [C,L,R,N]=test_rectangular_matrix(A)
+function [C,L,R,N,Apinv]=test_rectangular_matrix(A)
 r=rank(A), pause, disp(' ');
 [C,L]=QRcheck(A,r),  pause, disp(' ')
 [R,N]=QRcheck(A',r), pause, disp(' ')
