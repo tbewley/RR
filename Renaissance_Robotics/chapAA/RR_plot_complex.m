8f5748766e0fdff37fa246c47cf7a97b126832ce
Thomas Bewley <tbewley@ucsd.edu>
2024-09-19 07:32:42 -0600

    tweaked plot_complex and plot_real commands

--- Renaissance_Robotics/chapAA/RR_plot_complex.m
+++ Renaissance_Robotics/chapAA/RR_plot_complex.m
@@ -1,15 +1,22 @@
-function RR_plot_complex(z,c)
-% function RR_plot_complex(z,c)
-% This function just generates a nice plot, with line(s) of a specified color
-% from the origin to the complex value(s) in z 
+function RR_plot_complex(z,line,point)
+% function RR_plot_complex(z,line,point)
+% This function just generates a nice plot, with line(s) from the origin
+% to the complex value(s) in z, 
+% with the line(s) and the points(s) using a specified color and style
 % INPUT: z = scalar, vector, or matrix of complex values
-% TESTS: close all, z1=0.866+0.5*i, RR_plot_complex(z1,'b-')
-%        close all, RR_plot_complex(randn(5,3)+randn(5,3)*i,'b-')
+%        line = line style (e.g., 'b-')
+%        point = point style (e.g., 'bx')
+% TESTS: close all, z1=0.866+0.5*i, RR_plot_complex(z1,'b-.','bx')
+%        close all, RR_plot_complex(randn(5,3)+randn(5,3)*i,'r-','rx')
 %% Renaissance Repository, https://github.com/tbewley/RR (Renaissance Robotics, Appendix A)
 %% Copyright 2024 by Thomas Bewley, published under BSD 3-Clause License. 
 
-if nargin<2, c='k-', end
+if nargin<2, line='k-', end
+if nargin<3, point='kx', end
 origin=0
-[m,n]=size(z); if m*n>1, hold on, end
-for i=1:m; for j=1:n, plot([origin z(i,j)],c), end, end
+[m,n]=size(z); hold on
+for i=1:m; for j=1:n
+   plot([origin z(i,j)],line,'LineWidth',2)
+   plot([z(i,j)],      point,'LineWidth',2,'MarkerSize',15)
+end, end
 axis equal, grid on
--- Renaissance_Robotics/chapAA/RR_plot_real.m
+++ Renaissance_Robotics/chapAA/RR_plot_real.m
@@ -1,20 +1,33 @@
-function RR_plot_real(z,c)
-% This function just generates a nice plot, with line(s) of a specified color
-% from the origin to the 2D or 3D vector (or matrix of several 2D or 3D vectors) in z   
+function RR_plot_real(z,line,point)
+% function RR_plot_real(z,line,point)
+% This function just generates a nice plot, with line(s) from the origin
+% to the 2D or 3D vector (or matrix of several 2D or 3D vectors) in z,
+% with the line(s) and the points(s) using a specified color and style
 % INPUT: z = Nx1 vector, or Nxn matrix, of real values, where N=2 or 3
-% TESTS: close all, z1=[0.866; 0.5], RR_plot_real(z1,'b-')
-%        close all, RR_plot_real(randn(2,15),'b-')
-%        close all, RR_plot_real(randn(3,15),'b-')
+%        line = line style (e.g., 'b-')
+%        point = point style (e.g., 'bx')
+% TESTS: close all, z1=[0.866; 0.5], RR_plot_real(z1,'b-.','bx')
+%        close all, RR_plot_real(randn(2,15),'r-','ro')
+%        close all, RR_plot_real(randn(3,15),'k--','k*')
+%% Renaissance Repository, https://github.com/tbewley/RR (Renaissance Robotics, Appendix A)
+%% Copyright 2024 by Thomas Bewley, published under BSD 3-Clause License. 
 
 if nargin<2, c='k-', end
-[N,n]=size(z); if n>1, hold on, end
+if nargin<3, point='kx', end
+[N,n]=size(z); hold on
 if N==2
   origin=[0; 0];
-  for j=1:n, plot([origin(1) z(1,j)],[origin(2) z(2,j)],c), end
+  for j=1:n
+  	plot([origin(1) z(1,j)],[origin(2) z(2,j)],line,'LineWidth',2)
+  	plot(z(1,j),z(2,j),point,'LineWidth',2,'MarkerSize',15)
+  end
   axis equal, grid on
 elseif N==3
   origin=[0; 0; 0];
-  for j=1:n, plot3([origin(1) z(1,j)],[origin(2) z(2,j)],[origin(3) z(3,j)],c), end
+  for j=1:n
+  	plot3([origin(1) z(1,j)],[origin(2) z(2,j)],[origin(3) z(3,j)],line)
+  	plot3(z(1,j),z(2,j),z(3,j),point)
+  end
   view(45,20)
 else
   error('Need z to have 2 or 3 rows')
