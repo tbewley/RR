3b7edc142b2c87144b2fd38c18fcd3411a3fe2f8
Thomas Bewley <tbewley@ucsd.edu>
2024-10-29 14:54:56 -0600

    tweaks

--- me396/hello_world.c
+++ me396/hello_world.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+// Every good directory of codes needs this time-honored classic.  
+// Compile and run on most machines with:
+//    gcc hello_world.c -o hello_world; hello_world
+// I am seriously not copyrighting this one...  - Tom Bewley
 int main() {
-   // printf() displays the string inside quotation
    printf("Hello, World!\n");
    return 0;
 }
