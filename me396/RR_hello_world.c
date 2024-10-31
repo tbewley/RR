da3cdad2a302f5b9ef6cbd1b1d613aa7e6f839a4
Thomas Bewley <tbewley@ucsd.edu>
2024-10-31 06:04:56 -0600

    tweaks

--- me396/RR_hello_world.c
+++ me396/RR_hello_world.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-// Every good directory of codes needs this time-honored classic.  
+// Every good repository of codes needs this time-honored classic.  
 // Compile and run on most machines with:
-//    gcc hello_world.c -o hello_world; hello_world
+//    gcc RR_hello_world.c -o RR_hello_world; ./RR_hello_world
 // I am seriously not copyrighting this one...  - Tom Bewley
 int main() {
    printf("Hello, World!\n");
