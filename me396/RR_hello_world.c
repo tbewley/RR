cd5087b6b121711762fa8a43c0614065a9ddc706
Thomas Bewley <tbewley@ucsd.edu>
2024-11-03 11:57:06 -0700

    tweaks

--- me396/RR_hello_world.c
+++ me396/RR_hello_world.c
@@ -1,9 +1,14 @@
-#include <stdio.h>
-// Every good repository of codes needs this time-honored classic.  
+// Every good repository of codes needs some variant of this time-honored classic.  
 // Compile and run on most machines with:
 //    gcc RR_hello_world.c -o RR_hello_world; ./RR_hello_world
 // I am seriously not copyrighting this one...  - Tom Bewley
+
+#include <stdio.h>
+#include <unistd.h>
+
 int main() {
-   printf("Hello, World!\n");
+   int i=10;
+   while (i>=0) { printf("%d... ",i); fflush(stdout); sleep(1); i--; }
+   printf("\nHello, World!\n");
    return 0;
 }
