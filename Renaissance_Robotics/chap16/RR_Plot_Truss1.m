5032b5e5ba8ba1aef5c751725995687b1b6b7151
Thomas Bewley <tbewley@ucsd.edu>
2024-11-26 06:55:04 -0700

    truss stuff

--- Renaissance_Robotics/chap16/RR_Plot_Truss1.m
+++ Renaissance_Robotics/chap16/RR_Plot_Truss1.m
@@ -2,13 +2,14 @@ function RR_Plot_Truss1(Q,P,C,x,ax);
 N=[Q P]; [m,n]=size(C); [d,q]=size(Q); [d,p]=size(P);
 axis equal, axis tight, grid(ax), axis(ax,[-0.05 1.05 -0.3 0.4]), hold(ax,"on")
 h=max(Q(2,:));
-if h==0; fac=0; else, fac=1; end
+if h==0; fac=0; plot(ax,-0.035,min(Q(2,:))-.01,'w.');
+else,    fac=1; plot(ax,-0.035,h+.01,'w.'); end
 fill(ax,P(1,1)+[-.035     0 .035*fac],P(2,2)+[-.05 0 -.05],'k-')
 fill(ax,P(1,2)+[-.035*fac 0 .035    ],P(2,2)+[-.05 0 -.05],'k-')
-N=[Q P]; 
+N=[Q P];
 disp('In members: blue=tension, red=compression, black=no force')
-mn=min(x); fprintf('maximum tension     = %0.5g\n',mn)
-mx=max(x); fprintf('maximum compression = %0.5g\n',mx)
+mn=min(x); fprintf('maximum tension    (thick blue line) = %0.5g\n',mn)
+mx=max(x); fprintf('maximum compression (thick red line) = %0.5g\n',mx)
 for i=1:m
   [i1,d1]=max(C(i,:)); [i2,d2]=min(C(i,:)); lw=3; 
   if x(i)<-0.01,    sy='r-'; if x(i)<mn*0.95, lw=6; end
@@ -16,3 +17,5 @@ for i=1:m
   else, sy='k-'; end
   plot(ax,[N(1,d1) N(1,d2)],[N(2,d1) N(2,d2)],sy,"LineWidth",lw)
 end
+
+axis(ax,"tight"), axis(ax,"equal")
--- Renaissance_Robotics/chap16/RR_Truss_GUI.m
+++ Renaissance_Robotics/chap16/RR_Truss_GUI.m
@@ -2,7 +2,7 @@
 
 clear, figure(1), clf
 global truss height s load curvature ax
-truss="Pratt"; height=0.2; s=6; load=0.5; curvature=0;  % Initial values
+truss="Pennsylvania"; height=0.2; s=10; load=0.5; curvature=0;  % Initial values
 
 fig=uifigure; g=uigridlayout(fig);           % Set up a grid for all of the elements
 g.RowHeight = {'1x',40};                     % Tall upper row, fixed hight lower row
@@ -13,7 +13,7 @@ update_truss
 
 % truss choice
 dd = uidropdown(g,"ValueChangedFcn",@(src,event)update1(src,event), ...
-    "Items",["Warren","Howe","Pratt","Pennsylvania","K","Modified K"]);
+    "Items",["Warren","Howe","Pratt","Pennsylvania","K"]);
 dd.Layout.Row=2; dd.Layout.Column=1; dd.Value=truss;                        % This is in column 1 of row 2 of g 
 
 % height slider (relative height)
@@ -26,7 +26,7 @@ s2.Layout.Row=2; s2.Layout.Column=3; s2.Value=s;      s2.Limits=[2 15];     % Th
 
 % load slider (position along the bridge that the point load is applied)
 s3 = uislider(g,"ValueChangedFcn",@(src,event)update4(src,event));
-s3.Layout.Row=2; s3.Layout.Column=4; s3.Value=load;   s3.Limits=[0 1];      % This is in column 4 of row 2 of g
+s3.Layout.Row=2; s3.Layout.Column=4; s3.Value=load;   s3.Limits=[0.01 0.99];      % This is in column 4 of row 2 of g
 
 % curvature slider (controls curvature of top chord)
 s4 = uislider(g,"ValueChangedFcn",@(src,event)update5(src,event));  
@@ -42,12 +42,12 @@ function update5(src,event), global curvature; curvature=event.Value; update_tru
 function update_truss
 global truss height s load curvature ax
 
-cla(ax), hold on, s=round(s)
+cla(ax), hold on, s=round(s); clear Q U C;
 % Locations of the fixed nodes of the truss (note: normalized units)
 P=[0 1; 0 0]; p=2;  
 
 % Locations of the free nodes of the truss
-if truss=='Pennsylvania', sb=s*2; else, sb=s; end
+if strcmp(truss,'Pennsylvania'), sb=s*2; else, sb=s; end
 for i=1:sb-1, Q(:,i)=[i/sb; 0]; end, q=sb-1;   % free nodes in bottom row
 switch truss                                   
     case 'Warren'
@@ -57,10 +57,9 @@ switch truss
     case 'Pennsylvania'
         for i=1:s-1, Q(:,q+i)=[i/s; height*(1-curvature*4*(i/s-0.5)^2)]; end, q=q+s-1; % free nodes in top row
         Q(:,q+1)=[(1/2)/s;   Q(2,sb)/2]; q=q+1;
-        for i=2:s-1, Q(:,q+i)=[(i-1/2)/s; min(Q(2,sb+i-2),Q(2,sb+i-1))/2]; end; q=q+s-2; % free nodes in the middle row
+        for i=2:s-1, Q(:,q+i-1)=[(i-1/2)/s; min(Q(2,sb+i-2),Q(2,sb+i-1))/2]; end; q=q+s-2; % free nodes in the middle row
         Q(:,q+1)=[1-(1/2)/s; Q(2,sb)/2]; q=q+1;
     case 'K'
-    case 'Modified K'
 end
 n=q+p;
 
@@ -111,20 +110,34 @@ switch truss                        % free nodes in top row
         for i=1:num, C(j+i,s-i-1)=-1; C(j+i,2*s-i-1)=1; end, j=j+num; % up/right diagonals
         C(j+1,n-2)=-1; C(j+1,n)=1; j=j+1;                           % right diagonal to fixed node
     case 'Pennsylvania'
-        m=sb+3*s-3; C=zeros(m,q+p);      % FIX THIS.
-        C(1,n-1)=-1;   C(1,1)=1; j=1;                                % bottom row to left fixed node
-        for i=1:sb-2,  C(j+i,i)=-1; C(j+i,i+1)=1;     end, j=j+sb-2; % bottom row
-        C(j+1,sb-1)=-1; C(j+1,n)=1; j=j+1;                           % bottom row to right fixed node
-        for i=1:s-2,   C(j+i,s-1+i)=-1; C(j+i,s+i)=1; end, j=j+s-2; % top row
-        C(j+1,n-1)=-1; C(j+1,s)=1; j=j+1;                           % left diagonal to fixed node
-        for i=1:s-1,   C(j+i,i)=-1; C(j+i,s-1+i)=1;   end, j=j+s-1; % internal verticals
-        num=ceil((s-2)/2);
-        for i=1:num, C(j+i,i+1)=-1; C(j+i,s+i-1)=1;   end, j=j+num; % up/left  diagonals
-        for i=1:num, C(j+i,s-i-1)=-1; C(j+i,2*s-i-1)=1; end, j=j+num; % up/right diagonals
-        C(j+1,n-2)=-1; C(j+1,n)=1; j=j+1;                           % right diagonal to fixed node
+        m=9*s+1; if mod(s,2)==1, m=m+5; end, C=zeros(m,q+p);
+        C(1,n-1)=-1;    C(1,1)=1; j=1;                                  % bottom row to left fixed node
+        for i=1:sb-2,   C(j+i,i)=-1; C(j+i,i+1)=1;       end, j=j+sb-2; % bottom row
+        C(j+1,sb-1)=-1; C(j+1,n)=1; j=j+1;                              % bottom row to right fixed node
+        for i=1:s-2,    C(j+i,sb-1+i)=-1; C(j+i,sb+i)=1; end, j=j+s-2;  % top row
+        k=sb+s-2;
+        C(j+1,n-1)=-1;  C(j+1,k+1)=1;  C(j+2,n  ) =-1;  C(j+2,k+s)=1;    % left/right diagonal areas to fixed nodes
+        C(j+3,1  )=-1;  C(j+3,k+1)=1;  C(j+4,sb-1)=-1;  C(j+4,k+s)=1;
+        C(j+5,2)  =-1;  C(j+5,k+1)=1;  C(j+6,sb-2)=-1;  C(j+6,k+s)=1; 
+        C(j+7,sb) =-1;  C(j+7,k+1)=1;  C(j+8,k)=-1;     C(j+8,k+s)=1;  j=j+8; 
+        for i=1:s-1,    C(j+i,2*i)=-1; C(j+i,sb-1+i)=1;  end, j=j+s-1;  % internal verticals
+        num=floor(s/2);
+        for i=2:num
+            C(j+1,2*i-2) =-1;  C(j+1,k+i)=1; C(j+5,sb-2*i+2) =-1;  C(j+5,k+s+1-i)=1;    % left/right diagonal areas to fixed nodes
+            C(j+2,2*i-1) =-1;  C(j+2,k+i)=1; C(j+6,sb-2*i+1)=-1;   C(j+6,k+s+1-i)=1;
+            C(j+3,2*i)   =-1;  C(j+3,k+i)=1; C(j+7,sb-2*i  )=-1;   C(j+7,k+s+1-i)=1; 
+            C(j+4,sb+i-2)=-1;  C(j+4,k+i)=1; C(j+8,k+2-i)   =-1;   C(j+8,k+s+1-i)=1;
+            j=j+8; 
+        end
+        if mod(s,2)==1
+            C(j+1,s-1)      =-1;  C(j+1,k+num+1)=1;
+            C(j+2,s)        =-1;  C(j+2,k+num+1)=1;
+            C(j+3,s+1)      =-1;  C(j+3,k+num+1)=1;
+            C(j+4,2*s+num-1)=-1;  C(j+4,k+num+1)=1;
+            C(j+5,2*s+num)  =-1;  C(j+5,k+num+1)=1;
+        end
     case 'K'
-    case 'Modified K'
-end
+end, disp(' '); 
 
 % THE FOLLOWING TWO LINES IS WHERE THE MAGIC HAPPENS!  :)
 [A,u]=RR_Convert_DXCQ_eq_U_to_Ax_eq_u(Q,P,C,U);        % Convert the D*X*CQ=U problem to standard A*x=u form
@@ -132,7 +145,7 @@ x=pinv(A)*u;                 % Compute tensile and compressive forces x in the t
 
 RR_Plot_Truss1(Q,P,C,x,ax);  % Plot the truss (blue=tension, red=compression, black=no_force)
 if height>0, f=quiver(ax,load,0,0,0.1*(-1),0);           % Plot the load applied to the truss
-else,        f=quiver(ax,load,-0.1*(-1),0,fac*(-1));
+else,        f=quiver(ax,load,-0.1*(-1),0,0.11*(-1));
 end
 set(f,'MaxHeadSize',10000,'linewidth',3,'color','k'); % Set the arrow style
 
