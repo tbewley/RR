db7112542c01d84dddad67d9537542b72cbddda4
Thomas Bewley <tbewley@ucsd.edu>
2025-05-10 02:05:37 -0600

    3D frame case looking good!

--- Structural_Renaissance/chap5/RR_Frame_Signpost3D.m
+++ Structural_Renaissance/chap5/RR_Frame_Signpost3D.m
@@ -1,26 +1,25 @@
-% script RR_Frame_Signpost.m
+% script RR_Frame_Signpost3D.m
 % First, determine the locations of the nodes of the Signpost (in meters)
 %% Renaissance Repository, https://github.com/tbewley/RR (Structural Renaissance, Chapter 5)
 %% Copyright 2025 by Thomas Bewley, and published under the BSD 3-Clause LICENSE
 
-P=[ 0 1.5  1.5;  % Columns denote (x,y) locations of each of the p=2 Pinned support
-    0 0.5 -0.5;
+P=[ 0 1.5  1.5;  % Columns denote (x,y) locations of each of the p=3 Pinned support
+    0  1   -1;
     0  0    0]
-Q=[ 0 0 0 -2 1;   % (x,y) locations of the n=5 free nodes
-    0 0 0  0 0;
-    2 3 4  3 3];  
-U=[ 0 0 0  0 0;   % External forces applied to each of the n=5 free nodes (in Newtons)
-    0 0 0  0 0;
-    0 0 0 -1 0];  
-   % The m=5 structural Members of this pin-jointed frame are defined in the columns of CT.
-CT=[ 1 1 1 0 0 1;  % q_1   Each structural member connects several (Q,P,R) nodes of the frame, 
-     0 0 1 1 0 0;  % q_2   as indicated by the 1's in the corresponding columns at left.
-     0 0 1 0 1 0;  % q_3   Note: each structural member may connect 3 or more nodal points
-     0 0 0 1 1 0;  % q_4   (and, thus, each structural member may bear internal bending loads), 
-     0 0 0 1 0 1;  % q_5   and each node may have 3 or more strucural members attached,
-     0 0 1 0 0 0;  % p_1   in addition to having an externally applied load (defined in U).
-     1 0 0 0 0 0;  % p_2
-     0 1 0 0 0 0]; % p_3
+Q=[ 0 0 -2 1;   % (x,y) locations of the n=4 free nodes
+    0 0  0 0;
+    3 4  3 3];  
+U=[ 0 0 .2 0;   % External forces applied to each of the n=4 free nodes (in Newtons)
+    0 0 .2 0;
+    0 0 -1 0];  
+   % The m=8 structural Members of this pin-jointed frame are defined in the columns of CT.
+CT=[ 1 1 0 0 0 1 1 0;  % q_1   Each structural member connects several (Q,P,R) nodes of the frame,
+     0 0 1 0 0 1 0 1;  % q_2   as indicated by the 1's in the corresponding columns at left.
+     0 0 0 0 0 0 1 1;  % q_3   Note: each structural member may connect 3 or more nodal points 
+     0 0 1 1 1 0 1 0;  % q_4   (and, thus, each structural member may bear internal bending loads),
+     0 0 0 0 0 1 0 0;  % p_1   and each node may have 3 or more strucural members attached,
+     1 0 0 1 0 0 0 0;  % p_2   in addition to having an externally applied load (defined in U).
+     0 1 0 0 1 0 0 0]; % p_3
 % Now, convert the linear eqns for determining the static equilibrium of the frame into the
 % standard A*x=u form.  Note that these equations of static equilbrium are just the following:
 %   (a) the sum of forces on each pin = zero,
