09d00e1713a007d54158119a5a72c4003d4802de
Thomas Bewley <tbewley@ucsd.edu>
2025-05-16 09:39:10 -0600

    Update RR_Truss_USAFA_chapel_sub.m

--- Structural_Renaissance/chap6/RR_Truss_USAFA_chapel_sub.m
+++ Structural_Renaissance/chap6/RR_Truss_USAFA_chapel_sub.m
@@ -1,10 +1,10 @@
 P = [3.5115 3.5115; % x coord (m)
-     24.33  0;      % y coord (m)
-     0      0];     % z coord (m)
+     0      0;      % y coord (m)
+     24.33  0];     % z coord (m)
      % q1    q2    q3     q4    q5     q6      q7    q8     q9     q10    q11    q12    q13    q14   q15
 Q = [2.341  4.682  3.5115 1.170 5.8515 3.5115  0.000 7.0215 3.5115 1.170  5.5815 3.5115 2.341  4.682 3.5115; % x (m)
-     20.275 20.275 20.275 16.22 16.22  16.22  12.165 12.165 12.165 8.11   8.11   8.11   4.055  4.055 4.055;  % y (m)
-     -2.027 -2.027 0.00  -4.055 -4.055 0.00   -6.082 -6.082 0.00   -4.055 -4.055 0.00  -2.027 -2.027 0.00 ]; % z (m)
+     -2.027 -2.027 0.00  -4.055 -4.055 0.00   -6.082 -6.082 0.00   -4.055 -4.055 0.00  -2.027 -2.027 0.00  ; % y (m)
+     20.275 20.275 20.275 16.22 16.22  16.22  12.165 12.165 12.165 8.11   8.11   8.11   4.055  4.055 4.055]; % z (m)
 % define u matrix (applied force on nodes)
 % approximate each structural tube as 15kg = 150N
 % 100N = approx 30 mph winds --> approx 50N each in x and z directions
@@ -21,8 +21,7 @@ U3 = [x x x x x x x x x x x x x x x;  % applied x force (N)
       y y y y y y y y y y y y y y y;  % applied y force (N)
       z z z z z z z z z z z z z z z]; % applied z force (N)
 R = []; M = [];  S = [];
-p = 1; % nodes (p = fix, q = free)
-n = -1;
+p = 1; n = -1; % nodes (q=free, p=fixed)
   % q1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 p1 2
 C = [p 0 0 0 0 0 0 0 0 0 0 0 0 0 0  n 0; % m1
      0 p 0 0 0 0 0 0 0 0 0 0 0 0 0  n 0; % m2
