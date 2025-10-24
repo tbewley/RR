b9caa5c0e091f7b4967cbe8e1b871ada79a470a3
Thomas Bewley <tbewley@ucsd.edu>
2025-10-24 13:10:35 +0200

    Update RR_sphere_grid_C.m

--- Renaissance_Packings/chap03/RR_sphere_grid_C.m
+++ Renaissance_Packings/chap03/RR_sphere_grid_C.m
@@ -8,16 +8,16 @@
 %% Renaissance Repository, https://github.com/tbewley/RR (Renaissance Packings)
 %% Copyright 2025 by Thomas Bewley, and published under the BSD 3-Clause LICENSE
 
-clear, n=3  % n=2,3,4,... degree of refinement of each edge (N=2^n intervals)
+clear, n=2  % n=2,3,4,... degree of refinement of each edge (N=2^n intervals)
 omega=1.2     % amount of stretching (0=none, 1=regular stretching, 1.1=overstretching)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-N=2^n, global RR_VERBOSE; figure(1), clf, figure(2), clf
+N=1+2^n, global RR_VERBOSE; figure(1), clf, figure(2), clf
 fprintf('total gridpoints in spherical grid = %d\n',6*N^2)
 s=sqrt(3); % Cube construction 
 A=[ 1;1;1]/s; B=[ 1;1;-1]/s; C=[ 1;-1;-1]/s; D=[ 1;-1;1]/s;
 E=[-1;1;1]/s; F=[-1;1;-1]/s; G=[-1;-1;-1]/s; H=[-1;-1;1]/s;
 % Below we generate the 18 square and 8 triangular orthants of C
-[x,xR]=RR_sphere_gen_orthant(N,omega,0,A,B,C,D); [x,xR]=RR_sphere_gen_orthant(N,omega,0,E,F,G,H);
-[x,xR]=RR_sphere_gen_orthant(N,omega,0,A,B,F,E); [x,xR]=RR_sphere_gen_orthant(N,omega,0,B,C,G,F);
-[x,xR]=RR_sphere_gen_orthant(N,omega,0,C,D,H,G); [x,xR]=RR_sphere_gen_orthant(N,omega,0,D,A,E,H);
+[x,xR]=RR_sphere_gen_orthant(N,omega,1,A,B,C,D); [x,xR]=RR_sphere_gen_orthant(N,omega,1,E,F,G,H);
+[x,xR]=RR_sphere_gen_orthant(N,omega,1,A,B,F,E); [x,xR]=RR_sphere_gen_orthant(N,omega,1,B,C,G,F);
+[x,xR]=RR_sphere_gen_orthant(N,omega,1,C,D,H,G); [x,xR]=RR_sphere_gen_orthant(N,omega,1,D,A,E,H);
 view(57,19)
